de796cdd6ecd08efef4e82dd3d3b403d6e4d1d10
Luke Kurlandski <lk3591@g.rit.edu>
2023-08-22 10:04:12 -0400

    tokenization mis-match

--- run/tokenization.sh
+++ run/tokenization.sh
@@ -4,12 +4,12 @@
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
-#SBATCH --time=1-00:00:00
+#SBATCH --time=0-12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --ntasks=1
-#SBATCH --mem=64G
+#SBATCH --mem=128G
 
 
 if [ $# -eq 0 ]; then
@@ -17,14 +17,14 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-export vocab_size=$((2**$1 + 6))
+export vocab_size=$((2**$1))  # tokenization.py requires a log2 vocab_size
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate RawByteClf
 
 python src/tokenization.py \
---algorithm="Raw" \
+--algorithm="SentencePieceBPE" \
 --vocab_size=$vocab_size \
---num_files=1000 \
---block_size=2048 \
---batch_size=2048
+--num_files=4000 \
+--block_size=10000 \
+--batch_size=250
