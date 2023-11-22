7486fb0bc7b3920cb213f2748709f34613bbdcdb
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2023-11-22 11:27:54 -0500

    Generic train bash script for new system.

--- run/train.sh
+++ run/train.sh
@@ -11,36 +11,23 @@
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:a100:1
 
-if [ $# -eq 0 ]; then
-  echo "Please provide the log2 of the vocab size as an argument."
-  exit 1
-fi
-
-export vocab_size=$((2**$1 + 6))
-export steps=100
-export strategy="epoch"
-
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate RawByteClf
 
-# export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=1
 # export CUDA_LAUNCH_BLOCKING=1
+# export TRANSFORMERS_VERBOSITY="debug"
+
+strategy="epoch"
+steps=10
 
 #torchrun --standalone --nnodes=1 --nproc_per_node=1 \
 python \
-src/train.py \
---dataset_name="all" \
---vocab_size=$vocab_size \
---num_tok=1000 \
---num=1 \
---algorithm="Raw" \
---model="longformer" \
---max_length=10000 \
---scale_numerator=2 \
---scale_denominator=3 \
+src/learn/train.py \
+--root="./output" \
+--model_name_or_path="reformer" \
+--max_length=4096 \
 --task="clf" \
---pretrain_task="clf" \
---early_stopping \
 --do_train \
 --do_eval \
 --output_dir=tmp \
@@ -51,16 +38,16 @@ src/train.py \
 --evaluation_strategy=$strategy \
 --eval_steps=$steps \
 --logging_steps=$steps \
---dataloader_num_workers=14 \
---num_train_epochs=25 \
---per_device_train_batch_size=8 \
---per_device_eval_batch_size=16 \
---gradient_accumulation_steps=16 \
+--dataloader_num_workers=8 \
+--num_train_epochs=200 \
+--per_device_train_batch_size=64 \
+--per_device_eval_batch_size=64 \
+--gradient_accumulation_steps=4 \
 --optim="adamw_torch" \
---learning_rate="5e-5" \
+--learning_rate="1e-4" \
 --weight_decay=0.01 \
---group_by_length \
 --save_total_limit=5 \
 --fp16 \
---auto_find_batch_size \
---tf32=true  # REQUIRES =true
+--auto_find_batch_size
+#--group_by_length
+#--tf32=true  # REQUIRES =true
