4beffa1cccfd32ab7efa8cd788179bb0e9d9317b
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2023-12-13 09:58:30 -0500

    Added some code for oversampling but I don't think it works.

--- src/learn/utils.py
+++ src/learn/utils.py
@@ -2,13 +2,23 @@
 Utility functions for training and evaluation.
 """
 
-from collections import OrderedDict
+# pylint: disable=wrong-import-position
+print(f"Entered {__file__=}")
+
+from collections import Counter, OrderedDict
 import functools
 import gc
 import inspect
 import math
+import os
+from pprint import pprint
+import random
 import sys
 from typing import Any, Optional
+
+if __name__ == "__main__":
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+# pylint: enable=wrong-import-position
 
 from accelerate.utils.memory import should_reduce_batch_size
 from accelerate.utils import is_xpu_available
@@ -80,6 +90,42 @@ def preprocess_b(examples: dict[str, list]) -> dict[str, list]:
     return {
         "input_ids": [np.frombuffer(b, dtype=np.uint8) for b in examples["bytes"]],
     }
+
+
+def oversample_based_on_label(
+    examples: dict[str, list[Any]], probabilities: list[float]
+) -> dict[str, list[Any]]:
+    """Over sampling to approximately match the target class distribution.
+    
+    Args:
+        examples: A dictionary of lists of examples.
+        probabilities: The class distribution to match.
+    """
+
+    total_samples = len(examples["label"])
+    oversampled_examples = {key: [] for key in examples.keys()}
+    label_counter = Counter(examples["label"])
+
+    for label in label_counter.keys():
+        p = probabilities[label]
+
+        label_count = label_counter[label]
+        oversample_count = int((p * total_samples - label_count) / (1 - p))
+
+        label_indices = [i for i, value in enumerate(examples["label"]) if value == label]
+        oversampled_indices = random.choices(label_indices, k=oversample_count)
+
+        for key, values in examples.items():
+            oversampled_examples[key].extend([values[i] for i in oversampled_indices])
+
+    examples = {
+        k : (examples[k] + oversampled_examples[k])
+        if isinstance(examples[k], list)
+        else np.concatenate((examples[k], oversampled_examples[k]))
+        for k in examples
+    }
+
+    return examples
 
 
 def tokenize_fn(tokenizer: PreTrainedTokenizerFast, examples: Any, **kwds) -> dict[str, list]:
@@ -194,3 +240,56 @@ def str_or_bool_to_str(s: str | bool) -> bool:
         return False
 
     raise ValueError(f"Expected 'true', 'false', 'yes' or 'no', got {s}")
+
+
+def prob_norm(x: np.ndarray | list) -> np.ndarray | list:
+    if isinstance(x, np.ndarray):
+        return x / np.sum(x)
+    x = list(x)
+    s = sum(x)
+    return [i / s for i in x]
+
+
+def print_float_list(x: list[float], digits: int = 2) -> str:
+    return "[" + ", ".join([str(round(i, digits)) for i in x]) + "]"
+
+
+def round_list(x: list[float], digits: int = 2) -> list[float]:
+    return [round(i, digits) for i in x]
+
+
+def test_oversample_based_on_label():
+
+    C = 10
+    class_probabilities = prob_norm(np.random.rand(C))
+
+    for n in [10, 100, 1000, 10000, 100000, 1000000]:
+        examples = {"label": np.random.randint(0, 10, 100)}
+        oversampled_examples = oversample_based_on_label(examples, class_probabilities)
+
+        dist_old = Counter(examples["label"])
+        dist_new = Counter(oversampled_examples["label"])
+
+        dist_old = [x[1] for x in sorted(dist_old.items(), key=lambda x: x[0])]
+        dist_new = [x[1] for x in sorted(dist_new.items(), key=lambda x: x[0])]
+
+        p_old = prob_norm(dist_old)
+        p_new = prob_norm(dist_new)
+
+        distance_old = np.linalg.norm(class_probabilities - p_old)
+        distance_new = np.linalg.norm(class_probabilities - p_new)
+        delta = distance_old - distance_new
+
+        print(f"{n=}")
+        print(f"\t{distance_old=}")
+        print(f"\t{distance_new=}")
+        print(f"\t{delta=}")
+        print("-" * 80)
+
+        # print("class_probabilities:", round_list(class_probabilities), "\n")
+        # dist = sorted(Counter(examples["labels"]).items(), key=lambda x: x[0])
+        # print("original distribution:", dist, "\n")
+        # print("original probabilities:", round_list(prob_norm([d[1] for d in dist])), "\n")
+        # dist = sorted(Counter(oversampled_examples["labels"]).items(), key=lambda x: x[0])
+        # print("new distribution:", dist, "\n")
+        # print("new probabilities:", round_list(prob_norm([d[1] for d in dist])), "\n")
