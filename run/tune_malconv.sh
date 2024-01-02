85780a5d155d796e751cd2a7fd67f9d857942c5f
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-01-02 17:00:55 -0500

    Tuning for mymalconv at 2^26 token

--- run/tune_malconv.sh
+++ run/tune_malconv.sh
@@ -22,10 +22,11 @@ strategy="epoch"
 save_eval_steps=100
 logging_steps=10
 
+
 python \
 src/learn/train.py \
 --root="./output" \
---model_name_or_path="malconv" \
+--model_name_or_path="mymalconv" \
 --max_length=65536 \
 --task="clf" \
 --do_tune \
@@ -46,27 +47,51 @@ src/learn/train.py \
 --save_total_limit=5 \
 --fp16
 
+# python \
+# src/learn/train.py \
+# --root="./output" \
+# --model_name_or_path="malconv" \
+# --max_length=65536 \
+# --task="clf" \
+# --do_tune \
+# --output_dir=tmp \
+# --overwrite_output_dir \
+# --load_best_model_at_end \
+# --save_strategy=$strategy \
+# --save_steps=$save_eval_steps \
+# --evaluation_strategy=$strategy \
+# --eval_steps=$save_eval_steps \
+# --logging_steps=$logging_steps \
+# --dataloader_num_workers=2 \
+# --per_device_train_batch_size=256 \
+# --per_device_eval_batch_size=256 \
+# --num_train_epochs=1 \
+# --optim="adamw_torch" \
+# --learning_rate="5e-4" \
+# --save_total_limit=5 \
+# --fp16
 
-python \
-src/learn/train.py \
---root="./output" \
---model_name_or_path="malconvgct" \
---max_length=65536 \
---task="clf" \
---do_tune \
---output_dir=tmp \
---overwrite_output_dir \
---load_best_model_at_end \
---save_strategy=$strategy \
---save_steps=$save_eval_steps \
---evaluation_strategy=$strategy \
---eval_steps=$save_eval_steps \
---logging_steps=$logging_steps \
---dataloader_num_workers=2 \
---per_device_train_batch_size=256 \
---per_device_eval_batch_size=256 \
---num_train_epochs=1 \
---optim="adamw_torch" \
---learning_rate="5e-4" \
---save_total_limit=5 \
---fp16
+
+# python \
+# src/learn/train.py \
+# --root="./output" \
+# --model_name_or_path="malconvgct" \
+# --max_length=65536 \
+# --task="clf" \
+# --do_tune \
+# --output_dir=tmp \
+# --overwrite_output_dir \
+# --load_best_model_at_end \
+# --save_strategy=$strategy \
+# --save_steps=$save_eval_steps \
+# --evaluation_strategy=$strategy \
+# --eval_steps=$save_eval_steps \
+# --logging_steps=$logging_steps \
+# --dataloader_num_workers=2 \
+# --per_device_train_batch_size=256 \
+# --per_device_eval_batch_size=256 \
+# --num_train_epochs=1 \
+# --optim="adamw_torch" \
+# --learning_rate="5e-4" \
+# --save_total_limit=5 \
+# --fp16
