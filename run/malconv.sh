1e40db4378718f4e9966eda996709e786b16b346
Luke Kurlandski <lk3591@g.rit.edu>
2024-01-10 12:57:56 -0500

    Running mymalconv for T=2^20 bytes.

--- run/malconv.sh
+++ run/malconv.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=train
+#SBATCH --job-name=malconv
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
 #SBATCH --time=05-00:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=3
-#SBATCH --ntasks=9
-#SBATCH --mem=256G
-#SBATCH --gres=gpu:a100:4
+#SBATCH --cpus-per-task=4
+#SBATCH --ntasks=5
+#SBATCH --mem=128G
+#SBATCH --gres=gpu:a100:1
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate RawByteClf
 
-export CUDA_VISIBLE_DEVICES=1
+# export CUDA_VISIBLE_DEVICES=1
 # export CUDA_LAUNCH_BLOCKING=1
 # export TRANSFORMERS_VERBOSITY="info"
 
@@ -26,7 +26,7 @@ python \
 src/learn/train.py \
 --root="./output" \
 --model_name_or_path="mymalconv" \
---max_length=65536 \
+--max_length=1048576 \
 --task="clf" \
 --do_train \
 --do_eval \
@@ -38,7 +38,7 @@ src/learn/train.py \
 --evaluation_strategy=$strategy \
 --eval_steps=$save_eval_steps \
 --logging_steps=$logging_steps \
---dataloader_num_workers=2 \
+--dataloader_num_workers=4 \
 --per_device_train_batch_size=192 \
 --per_device_eval_batch_size=192 \
 --num_train_epochs=50 \
@@ -47,4 +47,4 @@ src/learn/train.py \
 --save_total_limit=3 \
 --fp16 \
 --fp16_full_eval \
---group_by_length
+--tf32=true
