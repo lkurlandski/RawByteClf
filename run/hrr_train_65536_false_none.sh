adaf1268b1eb5fac850b5991118f8d79a25c9da4
Luke Kurlandski <lk3591@g.rit.edu>
2024-01-24 09:42:20 -0500

    Configuration for HRR Classification Experiments with 10 classes.

--- run/hrr_train_65536_false_none.sh
+++ run/hrr_train_65536_false_none.sh
@@ -1,39 +1,36 @@
 #!/bin/bash -l
 
-#SBATCH --job-name=hrrformer_clf
+#SBATCH --job-name=top10_hrr_16384
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
-#SBATCH --time=05-00:00:00
+#SBATCH --time=01-00:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=5
-#SBATCH --mem=64G
+#SBATCH --mem=32G
 #SBATCH --gres=gpu:a100:1
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate RawByteClf
 
-torchrun --no-python --nnodes=1 --nproc_per_node=2 \
+# torchrun --no-python --nnodes=1 --nproc_per_node=2 \
 python \
 src/learn/train.py \
---root="./output" \
+--root="./output/top10" \
 --task="clf" \
 --streaming=false \
 --depth=1 \
 --do_train \
---do_eval \
 --output_dir=tmp \
 --overwrite_output_dir=true \
---save_strategy="steps" \
---evaluation_strategy="steps" \
---eval_steps=500 \
---save_steps=10 \
---logging_steps=1 \
+--save_strategy="epoch" \
+--evaluation_strategy="epoch" \
+--logging_steps=10 \
 --dataloader_num_workers=4 \
---num_train_epochs=5 \
+--num_train_epochs=25 \
 --optim="adamw_torch" \
---learning_rate="5e-4" \
+--learning_rate="1e-4" \
 --lr_scheduler_type="inverse_sqrt" \
 --warmup_steps=100 \
 --weight_decay=0.01 \
@@ -44,9 +41,10 @@ src/learn/train.py \
 --ft_freeze_positional_embeddings=false \
 --ft_duplicate_positional_embeddings=false \
 --ft_initialize_positional_embeddings=false \
---per_device_train_batch_size=16 \
---per_device_eval_batch_size=16 \
---gradient_accumulation_steps=4 \
+--per_device_train_batch_size=32 \
+--per_device_eval_batch_size=32 \
+--gradient_accumulation_steps=2 \
 --eval_accumulation_steps=16 \
 --fp16 \
---fp16_full_eval
+--fp16_full_eval \
+--tf32=true
