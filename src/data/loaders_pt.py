f993b6fe21434dbc632042409b40082e80c4cea6
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-01-28 15:14:22 -0500

    Fixed memory leak. Need to clean up and document.

--- src/data/loaders_pt.py
+++ src/data/loaders_pt.py
@@ -1,8 +1,5 @@
 """
 High-level loading API for pytorch datasets.
-
-# TODO:
-    - create a BinaryDatasetDict class that has common properties across different splits.
 """
 
 # pylint: disable=wrong-import-position
@@ -10,12 +7,16 @@ print(f"Entered {__file__=}")
 
 import asyncio
 from collections import Counter
+from collections.abc import Iterable, Sequence
 from datetime import datetime
-from itertools import islice
+import gc
+from itertools import cycle, islice
+import math
 import os
 from pathlib import Path
 import random
 import sys
+import time
 from typing import Callable, Literal, Optional
 
 if __name__ == "__main__":
@@ -25,6 +26,7 @@ if __name__ == "__main__":
 
 import numpy as np
 import pandas as pd
+import psutil
 import torch
 from torch.utils.data import ConcatDataset, Dataset, Subset, random_split
 from tqdm import tqdm
@@ -35,91 +37,144 @@ from src.data.loaders_hf import get_sorel_dataset as get_sorel_dataset_hf
 from src.data.prepare_datasets import s3_dataset_generator
 
 
-def read_binary_file(f: Path, max_length: Optional[int] = None, dtype: np.dtype = np.uint8) -> np.ndarray:
+def batched(iterable: Iterable, n: int):
+    it = iter(iterable)
+    while batch := tuple(islice(it, n)):
+        yield batch
+
+
+def read_binary_file(
+    f: Path,
+    max_length: Optional[int] = None,
+    dtype: Literal["bytes", "np", "pt"] = "bytes",
+) -> bytes | np.ndarray | torch.ByteTensor:
+    """
+    Args:
+        dtype: "UserWarning: The given buffer is not writable..."
+    """
     with open(f, "rb") as fp:
         b = fp.read(max_length)
-    x = np.frombuffer(b, dtype=dtype)
-    return x
+
+    if dtype == "bytes":
+        return b
+    elif dtype == "np":
+        return np.frombuffer(b, dtype=np.uint8)
+    elif dtype == "pt":
+        return torch.frombuffer(b, dtype=torch.uint8)
+
+    raise ValueError(f"Unknown {dtype=}")
 
 
-async def read_binary_file_async(f: Path, max_length: Optional[int] = None, dtype: np.dtype = np.uint8) -> np.ndarray:
+async def read_binary_file_async(
+    f: Path,
+    max_length: Optional[int] = None,
+    dtype: Literal["bytes", "np", "pt"] = "bytes",
+) -> bytes | np.ndarray | torch.ByteTensor:
     loop = asyncio.get_running_loop()
     return await loop.run_in_executor(None, read_binary_file, f, max_length, dtype)
 
 
+# FIXME: add len(SPECIALS) !!
 def preprocess_fn_add_cls_token(x: torch.LongTensor, cls_token_id: int) -> torch.LongTensor:
     return torch.cat([torch.tensor([cls_token_id], dtype=torch.long), x])
 
 
 class BinaryDataset(Dataset):
     """
-    files: list[Path]
-    labels: list[int]
-    max_length: int
-    keep_in_memory: bool
-    preprocess_fn: Callable[[torch.LongTensor], torch.LongTensor]
-    x: list[np.ndarray]
+    Loading the files asynchronously is signficantly faster, but seems to blow up.
 
-    asynchronous_loading is about 16x faster than single-threaded loading.
+    asyncronous_loading:
+        Asynchronous loading can be orders of magnitude faster, but has a quirky memory leak
+        (technically the memory leak could also exist in the synchronous version, but it seems
+        most liklely to be a result of my implementation of asynchronous loading). The memory
+        leak seems to be caused when reading a large number of files, e.g., it occurs when reading
+        600000 files at sequence length 512, but not for only 500000 files. The leak does not seem
+        connected to the number of bytes being read, e.g., it does not occur when reading 262144
+        bytes from 100000 files.
+
+    dtype:
+        "bytes" stores data as bytes
+        "np" stores data as a numpy.ndarray
+        "pt" stores data as a torch.ByteTensor
+        For short sequences, "bytes" requires marginally less memory than "np" and "pt", e.g.,
+        around 6% less for sequences of length 512. For long sequences, the overhead is negligible.
     """
 
     def __init__(
         self,
-        files: list[Path],
-        labels: Optional[list[int] | int] = None,
+        files: Iterable[os.PathLike] | Sequence[os.PathLike],
+        labels: Optional[Iterable[int] | Sequence[int]] = None,
         max_length: Optional[int] = None,
         keep_in_memory: bool = True,
         preprocess_fn: Callable[[torch.LongTensor], torch.LongTensor] = lambda x: x,
         id2label: Optional[dict[int, str]] = None,
         label2id: Optional[dict[str, int]] = None,
         asyncronous_loading: bool = True,
+        asynch_chunk_size: int = 100000,
+        in_memory_dtype: Literal["bytes", "np", "pt"] = "bytes",
+        length: Optional[int] = None,
     ) -> None:
-        self.files = files
-        if isinstance(labels, list):
-            if not isinstance(labels[0], int):
-                raise TypeError(f"labels must be a list of ints, not {type(labels[0])=}")
-            self.labels = labels
-        elif isinstance(labels, int):
-            self.labels = [labels] * len(files)
+
+        if hasattr(files, "__len__"):
+            self._length = len(files)
+        elif hasattr(labels, "__len__"):
+            self._length = len(labels)
+        elif length is not None:
+            self._length = length
         else:
-            self.labels = None
+            raise RuntimeError("Could not determine length of dataset.")
+
+        if not keep_in_memory:
+            if not isinstance(files, Sequence):
+                raise ValueError("Files must be a sequence if keep_in_memory=False.")
+            if self.labels is not None and not isinstance(labels, Sequence):
+                raise ValueError("Labels must be a sequence if keep_in_memory=False.")
+
+        self.files = files
+        self.labels = torch.tensor(labels, dtype=torch.long) if isinstance(labels, Sequence) else None
         self.max_length = max_length
         self.keep_in_memory = keep_in_memory
         self.preprocess_fn = preprocess_fn
+        self.in_memory_dtype = in_memory_dtype
+        self.asynch_chunk_size = asynch_chunk_size
+
+        self._id2label = id2label
+        self._label2id = label2id
+        self._dist = None
 
         if self.keep_in_memory:
             if asyncronous_loading:
                 loop = asyncio.get_event_loop()
                 loop.run_until_complete(self.load_dataset_into_memory_async())
             else:
-                self.x = []
-                for f in tqdm(self.files, desc="Loading dataset into memory..."):
-                    self.x.append(read_binary_file(f, max_length))
-
-        self._id2label = id2label
-        self._label2id = label2id
-        self._dist = None
+                x = []
+                for f in tqdm(files):
+                    x.append(read_binary_file(f, max_length, in_memory_dtype))
+                self.x = x
 
     def __getitem__(self, i: int) -> tuple[torch.LongTensor, torch.LongTensor]:
-        r = {"name": self.files[i].name if isinstance(self.files[i], Path) else self.files[i].split("/")[-1]}
+        r = {"name": str(self.files[i]).split("/")[-1]}
 
         if self.labels is not None:
-            y_i = self.labels[i]
-            y_i = torch.tensor(y_i, dtype=torch.long)
-            r["labels"] = y_i
+            r["labels"] = torch.tensor(self.labels[i], dtype=torch.long)
 
         if self.keep_in_memory:
             x_i = self.x[i]
+            if isinstance(x_i, bytes):
+                x_i: torch.ByteTensor = torch.frombuffer(x_i, dtype=torch.uint8)
+            elif isinstance(x_i, np.ndarray):
+                x_i: torch.ByteTensor = torch.from_numpy(x_i)
         else:
-            x_i = read_binary_file(self.files[i], self.max_length)
-        x_i = torch.tensor(x_i, dtype=torch.long)
+            x_i: torch.ByteTensor = read_binary_file(self.files[i], self.max_length, dtype="pt")
+
+        x_i: torch.LongTensor = x_i.to(torch.long)
         x_i = self.preprocess_fn(x_i)[0:self.max_length]
         r["input_ids"] = x_i
 
         return r
 
     def __len__(self) -> int:
-        return len(self.files)
+        return self._length
 
     def __repr__(self) -> str:
         return str(self)
@@ -136,8 +191,18 @@ class BinaryDataset(Dataset):
         )
 
     async def load_dataset_into_memory_async(self) -> list[np.ndarray]:
-        tasks = [read_binary_file_async(f, self.max_length) for f in self.files]
-        self.x = await asyncio.gather(*tasks)
+        file_chunks = batched(self.files, self.asynch_chunk_size)
+        # file_chunks = list(file_chunks)  # FIXME
+        n_chunks = math.ceil(len(self) / self.asynch_chunk_size)
+        desc = f"Asynchronously loading {len(self)} files in {n_chunks} chunks..."
+        x = []
+        for files in tqdm(file_chunks, desc=desc, total=n_chunks):
+            tasks = [read_binary_file_async(f, self.max_length, self.in_memory_dtype) for f in files]
+            x_i = await asyncio.gather(*tasks)
+            x.extend(x_i)
+            # gc.collect()
+        assert len(x) == len(self)
+        self.x = x
 
     @property
     def dist(self) -> Counter[str, int]:
@@ -337,6 +402,12 @@ def get_bodmas_dataset(
     }
     del files, labels
 
+    # Select a subset
+    files_and_labels = {
+        f: l for i, (f, l) in enumerate(files_and_labels.items())
+        if (not isinstance(subset, int) or i < subset)
+    }
+
     # Filter out the files that are not in the top_k most frequent labels
     dist: Counter[str, int] = Counter(files_and_labels.values())
     keep = [l for l, n in dist.most_common(top_k) if (n >= min_freq)]
@@ -417,13 +488,73 @@ def test():
     # print()
 
     # print(f"Time taken: {time.time() - start:.2f}s")
+    from argparse import ArgumentParser
+    import time
+    import math
 
+    parser = ArgumentParser()
+    parser.add_argument("--n_files", type=int, default=None)
+    parser.add_argument("--max_length", type=int, default=None)
+    parser.add_argument("--asynch", action="store_true")
+    parser.add_argument("--dtype", default="bytes")
+    args = parser.parse_args()
 
-    dataset = get_sorel_dataset(10000, 0.1, 0.1)
-    print(dataset)
-    for d in dataset["tr"]:
-        print(d)
-        break
+    BYTES_OBJ_OVERHEAD = 33
+
+    print(f"{args.n_files=}")
+    print(f"{args.max_length=}")
+    print(f"{args.asynch=}")
+    print(f"{args.dtype=}")
+
+    m_i = psutil.virtual_memory()
+    print(f"{m_i.total=}")
+    print(f"{m_i.used=}")
+
+    files = sorted(list(DATASET_TO_FILES["binaries"]["bodmas_pe"]()))
+    files = islice(cycle(files), args.n_files if isinstance(args.n_files, int) else len(files))
+
+    # files = list(files)  # FIXME
+    # print(f"{len(files)=}")
+
+    dataset = BinaryDataset(
+        files,
+        labels=None,
+        max_length=args.max_length,
+        keep_in_memory=True,
+        asyncronous_loading=args.asynch,
+        in_memory_dtype=args.dtype,
+        length=args.n_files,
+    )
+
+    m_f = psutil.virtual_memory()
+    print(f"{m_f.total=}")
+    print(f"{m_f.used=}")
+
+    isinstance(dataset, BinaryDataset)
+
+    true_mem_delta = m_f.used - m_i.used
+    expt_mem_delta = sum(
+        min(f.stat().st_size, args.max_length if isinstance(args.max_length, int) else sys.maxsize) + BYTES_OBJ_OVERHEAD
+        for f in files
+    )
+    mem_discrepancy = true_mem_delta - expt_mem_delta
+
+    print(f"{true_mem_delta=}")
+    print(f"{expt_mem_delta=}")
+    print(f"{mem_discrepancy=}")
+
+    # p = Path("./mem.log")
+    # if not p.exists():
+    #     p.write_text("subset,max_length,true_mem_delta,expt_mem_delta,mem_discrepancy\n")
+    # with open(p, "a") as fp:
+    #     fp.write(f"{SUBSET},{MAX_LENGTH},{true_mem_delta},{expt_mem_delta},{mem_discrepancy}\n") 
+
+    # print(dataset)
+    # for d in dataset["tr"]:
+    #     print(d)
+    #     break
+    
+    time.sleep(2)
 
 
 if __name__ == "__main__":
