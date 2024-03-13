a40a084c43cd3b67b68c1d3a13c7c933808e5331
Luke Kurlandski <lk3591@g.rit.edu>
2024-03-13 11:22:52 -0400

    Experiments

--- run/train_sizes/create.py
+++ run/train_sizes/create.py
@@ -1,6 +1,13 @@
 import math
 from pathlib import Path
 from pprint import pformat
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+from src.utils import get_highest_path
+
 
 BODY = """#!/bin/bash -l
 
@@ -8,7 +15,7 @@ BODY = """#!/bin/bash -l
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
-#SBATCH --time=DD-HH:00:00
+#SBATCH --time=01-00:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
@@ -19,19 +26,6 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate RawByteClf
 conda activate RawByteClf2
 module unload blindfold
-
-
-if [ $# -eq 0 ]; then
-    echo "Error: No argument supplied."
-    exit 1
-elif [ "$1" != "clf" ] && [ "$1" != "ftclf" ]; then
-    echo "Error: Invalid argument. Argument must be 'clf' or 'ftclf'."
-    exit 1
-elif [ "$1" = "clf" ]; then
-    MODEL_NAME_OR_PATH="mamba"
-elif [ "$1" = "ftclf" ]; then
-    MODEL_NAME_OR_PATH="PRETRAINED_MODEL_PATH"
-fi
 
 
 python -u \\
@@ -49,29 +43,27 @@ src/learn/train.py \\
 --do_train \\
 --do_eval \\
 --output_dir=tmp \\
---save_strategy="steps" \\
---evaluation_strategy="steps" \\
---save_steps=SAVE_EVAL_STEPS \\
---eval_steps=SAVE_EVAL_STEPS \\
---max_steps=17625 \\
+--save_strategy="epoch" \\
+--evaluation_strategy="epoch" \\
+--max_steps=20000 \\
 --logging_steps=10 \\
 --dataloader_num_workers=0 \\
 --optim="adamw_torch" \\
 --learning_rate="1e-3" \\
---lr_scheduler_type="linear" \\
---warmup_ratio=0.00 \\
+--lr_scheduler_type="inverse_sqrt" \\
+--warmup_ratio=0.01 \\
 --weight_decay=0.01 \\
 --adam_beta2=0.999 \\
 --max_grad_norm=1.0 \\
 --save_total_limit=3 \\
---model_name_or_path=$MODEL_NAME_OR_PATH \\
---max_length=512 \\
+--model_name_or_path=MODEL_NAME_OR_PATH \\
+--max_length=MAX_LENGTH \\
 --per_device_train_batch_size=64 \\
 --per_device_eval_batch_size=64 \\
 --gradient_accumulation_steps=1 \\
 --load_best_model_at_end \\
---early_stopping=true \\
---early_stopping_patience=EARLY_STOPPING_PATIENCE \\
+--early_stopping=false \\
+--early_stopping_patience=3 \\
 --early_stopping_threshold=0 \\
 --auto_find_batch_size_and_gradient_accumulation_steps \\
 --bf16 \\
@@ -79,59 +71,40 @@ src/learn/train.py \\
 --tf32=true
 """
 
-# n_layers, d_model, n_params, time DD:HH
+
 CONFIGS = [
-    (8, 384,   7815552, "00-01"),
-    (12, 384, 11672448, "00-01"),
-    (12, 512, 20478464, "00-01"),
-    (16, 512, 27259392, "00-01"),
-    (24, 512, 40821248, "00-02"),
-    (24, 768, 90723072, "00-04"),
+    (4, 192, 1.057728, "00-02"),
+    (8, 384, 7.815552, "00-06"),
+    (12, 384, 11.672448, "00-06"),
+    (16, 512, 27.259392, "00-12"),
 ]
+TR_SIZES = [50, 100, 150, 200, 250, 500, 750, 1000, 1500, 2000, 3000, 4000, 5000]
 
 
-
-TR_SIZES = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200, 250, 300, 450, 500, 600, 700, 800, 900, 1000]
-
-
+MAX_LENGTH=16384
 OUTPUT = Path("run/train_sizes")
-PRETRAINED_MODELS_ROOT = Path("/home/lk3591/Documents/code/RawByteClf/output/scaling/mamba/512/clm/3000000/1/")
-
-
-def dd_hh(p: int) -> str:
-    hours = math.ceil((p / 5e6) * 6)  # 6 hours for every 5 million parameters
-    days = 0
-    while hours >= 24:
-        hours -= 24
-        days += 1
-    if days > 5:
-        days = 5
-        hours = 0
-    return f"0{days}-{hours}"
+PRETRAINED_MODELS_ROOT = Path(
+    "/home/lk3591/Documents/code/RawByteClf/output/"
+    f"scaling/mamba/{MAX_LENGTH}/clm/7000000/1/"
+)
 
 
 def save_eval_steps(size: int) -> int:
-    if size < 50:
-        return 5
-    if size < 100:
-        return 10
-    if size < 200:
-        return 50
-    if size < 500:
-        return 100
-    return 200
+    if size > 1000:
+        return int(size / 100)
+    return 
 
 
 def early_stopping_patience(size: int) -> int:
     if size < 50:
-        return 25
+        return 50
     if size < 100:
-        return 20
+        return 40
     if size < 200:
-        return 15
+        return 30
     if size < 500:
-        return 10
-    return 5
+        return 20
+    return 10
 
 
 def get_pretrained_model_path(d_model: int, n_layer: int) -> Path:
@@ -152,41 +125,38 @@ def get_pretrained_model_path(d_model: int, n_layer: int) -> Path:
         elif "clf" not in str(pretrained_model_paths[1]):
             pretrained_model_path = pretrained_model_paths[1]
 
-    pretrained_model_path: Path = pretrained_model_path / "checkpoint-1400"
+    pretrained_model_path: Path = get_highest_path(pretrained_model_path, lstrip="checkpoint-")
     return pretrained_model_path
 
 
 outfiles = []
-
-
-for n_layer, d_model, params, req_time in CONFIGS:
+for n_layer, d_model, params, dd_hh in CONFIGS:
     for tr_size in TR_SIZES:
-        jobname = f"tr_sz_bl_{tr_size}_{n_layer}-{d_model}"
-        print(jobname)
+        for finetune in [True, False]:
+            jobname = f"tr_sz_bl_{MAX_LENGTH}_{finetune}_{tr_size}_{n_layer}_{d_model}"
+            print(jobname)
 
-        pretrained_model_path = get_pretrained_model_path(d_model, n_layer)
-        assert pretrained_model_path.exists(), pretrained_model_path
+            pretrained_model_path = get_pretrained_model_path(d_model, n_layer)
+            assert pretrained_model_path.exists(), pretrained_model_path
 
-        # must replace PRETRAINED_MODEL_PATH before D_MODEL!
-        text = BODY \
+            model_name_or_path = pretrained_model_path.as_posix() if finetune else "mamba"
+            text = BODY \
             .replace("JOB_NAME", jobname) \
-            .replace("PRETRAINED_MODEL_PATH", pretrained_model_path.as_posix()) \
+            .replace("MODEL_NAME_OR_PATH", pretrained_model_path.as_posix()) \
             .replace("D_MODEL", str(d_model)) \
             .replace("N_LAYER", str(n_layer)) \
-            .replace("DD-HH", req_time) \
             .replace("TR_SIZE", str(tr_size)) \
             .replace("BODMAS_TOP_K", str(10)) \
-            .replace("EARLY_STOPPING_PATIENCE", str(early_stopping_patience(tr_size))) \
-            .replace("SAVE_EVAL_STEPS", str(save_eval_steps(tr_size)))
+            .replace("MAX_LENGTH", str(MAX_LENGTH)) \
+            .replace("DD-HH", dd_hh)
 
-        outfile = OUTPUT / f"{jobname}.sh"
-        with open(outfile, "w") as fp:
-            fp.write(text)
-        outfiles.append(outfile)
+            outfile = OUTPUT / f"{jobname}.sh"
+            with open(outfile, "w") as fp:
+                fp.write(text)
+            outfiles.append(outfile)
 
 
 with open(OUTPUT / "run.sh", "w") as fp:
-    for outfile in sorted(outfiles, key=lambda p: int(p.as_posix().split("_")[4])):
-        fp.write(f"sbatch {outfile.as_posix()} clf\n")
-        fp.write(f"sbatch {outfile.as_posix()} ftclf\n")
+    for outfile in sorted(outfiles, key=lambda p: int(p.as_posix().split("_")[7])):
+        fp.write(f"sbatch {outfile.as_posix()}\n")
 
