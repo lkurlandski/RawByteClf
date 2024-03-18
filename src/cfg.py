6b589ea6d36e996c842368d6d56fd7c56eb52acc
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-03-18 10:36:48 -0400

    tokenizers path

--- src/cfg.py
+++ src/cfg.py
@@ -18,5 +18,6 @@ BR = "|" + "-" * 88 + "|"
 
 INPUT_PATH = Path("./input")
 OUTPUT_PATH = Path("./output")
+TOKENIZERS_OUTPUT_PATH = OUTPUT_PATH / "tokenizers"
 
 TMP_DIR = Path("/scratch.local") if Path("/scratch.local").exists() else Path(".")
