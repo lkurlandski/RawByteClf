05c3b3341e648acb9ca0dc649cdc289cc7878fe5
Luke Kurlandski <lk3591@g.rit.edu>
2024-04-12 11:42:03 -0400

    Updated experiments.

--- run/finetuning/create.py
+++ run/finetuning/create.py
@@ -8,17 +8,17 @@ from typing import Optional
 
 ROOT = "./output/finetuning"
 MODEL_NAME_OR_PATH = "mamba"
-ARCH_CONFIG = '{"mode": "bi", "d_model": 128, "n_layer": 6, "mlp_hidden_size": 512}'
+ARCH_CONFIG = '{"mode": "uni", "d_model": 256, "n_layer": 8, "mlp_hidden_size": 512}'
 PER_DEVICE_TRAIN_BATCH_SIZE = 16
 PER_DEVICE_EVAL_BATCH_SIZE = 32
 REPRESENTATION = 8
 ALGORITHM = "Raw"
 VOCAB_SIZE = 256
-MAX_LENGTH = 2 ** 14
-DATA_READ_BYTES = 2 ** 14
-TOP_K = 10
-BF_OR_FP = "fp"
-TF32 = "false"
+MAX_LENGTH = 2 ** 16
+DATA_READ_BYTES = 2 ** 16
+TOP_K = 100
+BF_OR_FP = "bf"
+TF32 = "true"
 
 
 BODY_CLM = f"""#!/bin/bash -l
@@ -27,7 +27,7 @@ BODY_CLM = f"""#!/bin/bash -l
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
-#SBATCH --time=01-00:00:00
+#SBATCH --time=02-00:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks={4}
@@ -48,7 +48,7 @@ src/learn/train.py \\
 --metric_for_best_model="eval_loss" \\
 --task="clm" \\
 --streaming=true \\
---skip_eval_check=true \\
+--skip_eval_check=false \\
 --dataset_backend="HF" \\
 --representation={REPRESENTATION} \\
 --algorithm={ALGORITHM} \\
@@ -62,8 +62,8 @@ src/learn/train.py \\
 --evaluation_strategy="steps" \\
 --num_train_epochs=1 \\
 --logging_steps=10 \\
---save_steps=100 \\
---eval_steps=100 \\
+--save_steps=400 \\
+--eval_steps=400 \\
 --dataloader_num_workers={3} \\
 --optim="adamw_torch" \\
 --learning_rate="1e-3" \\
@@ -94,7 +94,7 @@ BODY_CLF = f"""#!/bin/bash -l
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
-#SBATCH --time=01-00:00:00
+#SBATCH --time=00-8:00:00
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks={4}
@@ -146,7 +146,7 @@ src/learn/train.py \\
 --data_read_bytes={DATA_READ_BYTES} \\
 --per_device_train_batch_size={PER_DEVICE_TRAIN_BATCH_SIZE} \\
 --per_device_eval_batch_size={PER_DEVICE_EVAL_BATCH_SIZE} \\
---gradient_accumulation_steps=1 \\
+--gradient_accumulation_steps=4 \\
 --load_best_model_at_end \\
 --early_stopping=false \\
 --auto_find_batch_size_and_gradient_accumulation_steps \\
@@ -156,14 +156,20 @@ src/learn/train.py \\
 """
 
 
-# BODY_FT = BODY_CLF.replace("ftCLF", "ftFT").replace("mamba", "")
-
-
 OUTPUT = Path(os.path.realpath(__file__)).parent
+CHECKPOINTS = Path("/home/lk3591/Documents/code/RawByteClf/output/finetuning/mamba/representation--8/algorithm--Raw/vocab_size--256/max_length--65536/task--clm/tr_size--1000000/depth--1/mode--uni/d_model--256/n_layer--8/mlp_hidden_size--512/per_device_train_batch_size--8/gradient_accumulation_steps--32/learning_rate--0.001/weight_decay--0.01/adam_beta1--0.9/adam_beta2--0.999/adam_epsilon--1e-08/max_grad_norm--1.0/lr_scheduler_type--linear/warmup_ratio--0.0/bf16--True/fp16--False/tf32--True/optim--adamw_torch/checkpoints/")
 
-with open(OUTPUT / "clm.sh", "w") as f:
-    f.write(BODY_CLM)
-with open(OUTPUT / "clf.sh", "w") as f:
-    f.write(BODY_CLF)
-# with open("ft.sh", "w") as f:
-#     ...
+
+with open(OUTPUT / "clm.sh", "w") as fp:
+    fp.write(BODY_CLM)
+with open(OUTPUT / "clf.sh", "w") as fp:
+    fp.write(BODY_CLF)
+
+
+for checkpoint in CHECKPOINTS.glob("checkpoint-*"):
+    steps = checkpoint.stem.replace("checkpoint-", "")
+    body = BODY_CLF \
+        .replace("mamba", checkpoint.as_posix()) \
+        .replace("ftCLF", f"ftFTCLF-{steps}")
+    with open(OUTPUT / f"ftclf-{steps}.sh", "w") as fp:
+        fp.write(body)
