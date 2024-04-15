a4721143f66b78525c1b15752c416197a6d24038
Luke Kurlandski <lk3591@g.rit.edu>
2024-04-15 13:58:34 -0400

    use_reentrant for gradient checkpointing

--- run/finetuning/create.py
+++ run/finetuning/create.py
@@ -6,6 +6,11 @@ from pprint import pformat
 from typing import Optional
 
 
+ALGORITHMS = ["BPE", "Unigram"]
+VOCAB_SIZES = [1024, 4096, 16384, 65536]
+
+
+JOB_NAME = "JOB_NAME"
 ROOT = "./output/finetuning"
 MODEL_NAME_OR_PATH = "mamba"
 ARCH_CONFIG = '{"mode": "uni", "d_model": 256, "n_layer": 8}'
@@ -14,18 +19,18 @@ PER_DEVICE_EVAL_BATCH_SIZE = 64
 CLM_GRADIENT_ACCUMULATION_STEPS = 8
 CLF_GRADIENT_ACCUMULATION_STEPS = 2
 REPRESENTATION = 8
-ALGORITHM = "Raw"
-VOCAB_SIZE = 256
+ALGORITHM = "ALGORITHM"
+VOCAB_SIZE = "VOCAB_SIZE"
 MAX_LENGTH = 2 ** 16
 DATA_READ_BYTES = 2 ** 16
-TOP_K = 10
+TOP_K = 100
 BF_OR_FP = "bf"
 TF32 = "true"
 
 
 BODY_CLM = f"""#!/bin/bash -l
 
-#SBATCH --job-name=ft2CLM
+#SBATCH --job-name={JOB_NAME}
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
@@ -93,7 +98,7 @@ src/learn/train.py \\
 
 BODY_CLF = f"""#!/bin/bash -l
 
-#SBATCH --job-name=ft2CLF
+#SBATCH --job-name={JOB_NAME}
 #SBATCH --account=admalware
 #SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
@@ -161,20 +166,30 @@ src/learn/train.py \\
 
 
 OUTPUT = Path(os.path.realpath(__file__)).parent
-CHECKPOINTS = [
-    "/home/lk3591/Documents/code/RawByteClf/output/finetuning/mamba/representation--8/algorithm--Raw/vocab_size--256/max_length--65536/task--clm/tr_size--1000000/depth--1/mode--uni/d_model--256/n_layer--8/per_device_train_batch_size--16/gradient_accumulation_steps--16/learning_rate--0.001/weight_decay--0.01/adam_beta1--0.9/adam_beta2--0.999/adam_epsilon--1e-08/max_grad_norm--1.0/lr_scheduler_type--linear/warmup_ratio--0.0/bf16--True/fp16--False/tf32--True/optim--adamw_torch/checkpoints/CHECKPOINT-1000/",
-]
 
 
-with open(OUTPUT / "clm.sh", "w") as fp:
-    fp.write(BODY_CLM)
-with open(OUTPUT / "clf.sh", "w") as fp:
-    fp.write(BODY_CLF)
-for checkpoint in CHECKPOINTS:
-    checkpoint = Path(checkpoint)
-    steps = checkpoint.stem.replace("CHECKPOINT-", "")
-    body = BODY_CLF \
-        .replace("mamba", checkpoint.as_posix()) \
-        .replace("ft2CLF", f"ft2FTCLF-{steps}")
-    with open(OUTPUT / f"ftclf-{steps}.sh", "w") as fp:
-        fp.write(body)
+for algorithm in ALGORITHMS:
+    for vocab_size in VOCAB_SIZES: 
+        job_name = f"ftCLM-{algorithm[0]}-{vocab_size}"
+        body = BODY_CLM \
+            .replace("JOB_NAME", job_name) \
+            .replace("ALGORITHM", algorithm) \
+            .replace("VOCAB_SIZE", str(vocab_size))
+        with open(OUTPUT / f"{job_name}.sh", "w") as fp:
+            fp.write(body)
+
+        job_name = f"ftCLF-{algorithm[0]}-{vocab_size}"
+        body = BODY_CLF \
+            .replace("JOB_NAME", job_name) \
+            .replace("ALGORITHM", algorithm) \
+            .replace("VOCAB_SIZE", str(vocab_size))
+        with open(OUTPUT / f"{job_name}.sh", "w") as fp:
+            fp.write(body)
+
+        job_name = f"ftCLFFT-{algorithm[0]}-{vocab_size}"
+        pretrained_model_path = "FIXME"
+        body = body.replace("mamba", pretrained_model_path)
+        with open(OUTPUT / f"{job_name}.sh", "w") as fp:
+            fp.write(body)
+
+
