dc527b69bb2b7181c5d59411c2acf67b5f9c093b
Luke Kurlandski <lk3591@g.rit.edu>
2024-04-23 11:23:13 -0400

    Updated to newest download script

--- run/download_sorel.sh
+++ run/download_sorel.sh
@@ -12,13 +12,14 @@
 
 
 source ~/anaconda3/etc/profile.d/conda.sh
-conda activate RawByteClf
+conda activate RawByteClf2
+
 
 python -u src/data/download_sorel.py \
 --output_root="/home/lk3591/Documents/datasets/Sorel/binaries" \
---num_samples=10000000 \
+--num_samples=2000000 \
 --num_bytes=1048576 \
 --max_length=1048576 \
 --shard_idx=$1 \
---num_shards=2000 \
+--num_shards=1000 \
 --errors=2
