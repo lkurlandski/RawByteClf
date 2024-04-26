d5fd9cccd805fd4cf8f113d7f97fc00cccb9e957
lkurlandski <lk3591@g.rit.edu>
2024-04-26 12:03:58 -0400

    Bug fix where AWS was getting killed too quick

--- run/detect_packing_sorel.sh
+++ run/detect_packing_sorel.sh
@@ -1,36 +1,42 @@
+#!/bin/bash -l
+#
+#
+#
+
 
 run() {
     local filter="$1"
 
+    echo "$filter: Creating..."
     directory="/tmp/sorel/$filter"
-    mkdir -p $directory
+    mkdir $directory
 
     echo "$filter: Downloading..."
     aws s3 cp s3://sorel-20m/09-DEC-2020/binaries/ $directory --no-sign-request --exclude="*" --include="$filter*" --recursive > "logs/packing_aws_$filter.log" 2>&1 &
     aws_pid=$!
+    sleep 15
 
-    sleep 1
-
-    while ! grep -q "Completed" "logs/packing_aws_$filter.log"; do
-        # Check if the aws process is still running
-        if ! ps -p $aws_pid > /dev/null; then
-            echo "The aws s3 cp command terminated unexpectedly."
-            exit 1
+    # Busy wait until the log file size remains unchanges,
+    # indicating that the AWS process has completed but is hanging.
+    initial_size=$(wc -l "logs/packing_aws_$filter.log")
+    while :
+    do
+        current_size=$(wc -l %s "logs/packing_aws_$filter.log")
+        if [ "$current_size" -eq "$initial_size" ]; then
+            echo "Log file size remains unchanged. AWS process has completed."
+            break
         fi
-        # Sleep for a short duration before checking again
-        sleep 1
+        initial_size=$current_size
+        sleep 2
     done
     kill $aws_pid
 
-    sleep 1
-
-    echo "$filter: Extracting..."
+    num_downloaded=$(ls $directory | wc -l)
+    echo "$filter: Extracting $num_downloaded..."
     for file in "$directory"/*; do
-
         if [[ $file == *"."* ]]; then
             continue  # Skip files with a suffix
         fi
-
         zlib-flate -uncompress < "$file" > "${file}.exe"
         rm $file
     done
@@ -42,17 +48,64 @@ run() {
 
     echo "$filter: Cleaning..."
     rm -rf $directory
-    rm "$directory/*" > /dev/null 2>&1
+    rm $directory/* > /dev/null 2>&1
     rmdir $directory > /dev/null 2>&1
 
     kill -9 $aws_pid  > /dev/null 2>&1
 }
 
-export -f run
-
-
-for ((i=0; i<4096; i++)); do
-    filter=$(printf "%03x" "$i")
+test() {
+    local filter="$1"
     echo "$filter"
-done | parallel --max-procs 252 run
+}
+
+export -f run
+export -f test
+
+pkill aws > /dev/null 2>&1
+pkill diec > /dev/null 2>&1
+pkill zlib-flate > /dev/null 2>&1
+
+rm /home/lk3591/Documents/datasets/Sorel/diec/* > /dev/null 2>&1
+rm /home/lk3591/Documents/code/RawByteClf/logs/packing_aws_* > /dev/null 2>&1
+rm -rf /tmp/sorel/* > /dev/null 2>&1
+
+JOBS=1024
+ULIMIT=16384
+
+currentLimit=$(ulimit -n)
+echo "Current ULimit: $currentLimit"
+ulimit -n $ULIMIT
+currentLimit=$(ulimit -n)
+echo "New ulimit: $currentLimit"
+
+# 
+# for ((i=0; i<256; i++)); do
+#     filter=$(printf "%02x" "$i")
+#     echo "$filter"
+# done | parallel --jobs $JOBS run
+
+# Takes ~10 seconds to connect plus ~100 seconds per aws batch for ~2450 files (2.1GiB)
+# for ((i=0; i<4096; i++)); do
+#     filter=$(printf "%03x" "$i")
+#     echo "$filter"
+# done | parallel --jobs $JOBS run
+
+# Takes ~10 seconds to connect plus ~10 seconds per aws batch for ~175 files (0.2GiB)
+for ((i=0; i<65536; i++)); do
+    filter=$(printf "%04x" "$i")
+    echo "$filter"
+done | parallel --jobs $JOBS run
+
+
+# echo "sha,status" > /home/lk3591/Documents/datasets/Sorel/packing.csv
+
+
+# for file in /home/lk3591/Documents/datasets/Sorel/diec/*.txt; do
+#     sha=$(basename "$file" | cut -d '.' -f 1)
+#     echo "$sha"
+#     status=$(jq -r '.status' "$file")
+#     echo "$status"
+#     echo "$sha,$status" >> output.csv
+# done
 
