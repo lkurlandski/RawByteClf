69267bc6e4fccfcb674ec8f490e0848f7950f381
lkurlandski <lk3591@g.rit.edu>
2024-05-01 08:32:05 -0400

    Final iteration of the detect packing script.

--- run/detect_packing_sorel.sh
+++ run/detect_packing_sorel.sh
@@ -3,55 +3,102 @@
 #
 #
 
+# Global variables must be exported to be available to parallel subprocesses
+export OUTPUT="/home/lk3591/Documents/datasets/Sorel/diec"
+export LOG="/home/lk3591/Documents/code/RawByteClf/logs"
+export DOWNLOAD="/tmp/sorel"
+
+export AWS_WAIT=5
+export DIEC_TIMEOUT=5
 
 run() {
-    local filter="$1"
+    t_start_all=$(date +%s)
 
-    echo "$filter: Creating..."
-    directory="/tmp/sorel/$filter"
+    local filter="$1"
+    logfile="$LOG/packing_$filter.log"
+    awslogfile="$LOG/packing_aws_$filter.log"
+    directory="$DOWNLOAD/$filter"
     mkdir $directory
 
-    echo "$filter: Downloading..."
-    aws s3 cp s3://sorel-20m/09-DEC-2020/binaries/ $directory --no-sign-request --exclude="*" --include="$filter*" --recursive > "logs/packing_aws_$filter.log" 2>&1 &
+    #echo "logfile: $logfile"
+    #echo "awslogile: $awslogfile"
+    #echo "directory: $directory"
+    # exit
+
+    echo "Downloading..." >> $logfile 2>&1
+    t_start=$(date +%s)
+    aws s3 cp s3://sorel-20m/09-DEC-2020/binaries/ $directory --no-sign-request --exclude="*" --include="$filter*" --recursive > $awslogfile 2>&1 &
     aws_pid=$!
-    sleep 15
 
-    # Busy wait until the log file size remains unchanges,
-    # indicating that the AWS process has completed but is hanging.
-    initial_size=$(wc -l "logs/packing_aws_$filter.log")
-    while :
-    do
-        current_size=$(wc -l %s "logs/packing_aws_$filter.log")
-        if [ "$current_size" -eq "$initial_size" ]; then
-            echo "Log file size remains unchanged. AWS process has completed."
-            break
-        fi
-        initial_size=$current_size
-        sleep 2
+    # Wait for aws to connect.
+    num=$(ls $directory | wc -l)
+    echo "Starting AWS ($num)..." >> $logfile 2>&1
+    while [ $num -eq 0 ]; do
+        sleep $AWS_WAIT
+	num=$(ls $directory | wc -l)
+	echo "Starting AWS ($num)..." >> $logfile 2>&1
     done
-    kill $aws_pid
 
-    num_downloaded=$(ls $directory | wc -l)
-    echo "$filter: Extracting $num_downloaded..."
-    for file in "$directory"/*; do
+    # Wait until aws hangs.
+    sizePrev="-1"
+    sizeCurr=$(wc -l < "$awslogfile")
+    echo "Runnning AWS ($sizeCurr, $sizePrev)..." >> $logfile 2>&1
+    while [ $sizeCurr -ne $sizePrev ]; do
+        sleep $AWS_WAIT
+        sizePrev=$sizeCurr
+        sizeCurr=$(wc -l < "$awslogfile")
+	echo "Runnning AWS ($sizeCurr, $sizePrev)..." >> $logfile 2>&1
+    done
+    sleep 5
+    kill $aws_pid
+    t_end=$(date +%s)
+    t_download=$((t_end-t_start))
+
+    num=$(ls $directory | wc -l)
+    echo "Extracting ($num)..." >> $logfile 2>&1
+    t_start=$(date +%s)
+    for file in $directory/*; do
         if [[ $file == *"."* ]]; then
-            continue  # Skip files with a suffix
+	    rm -rf $file
+            continue
         fi
         zlib-flate -uncompress < "$file" > "${file}.exe"
         rm $file
     done
+    t_end=$(date +%s)
+    t_extract=$((t_end-t_start))
 
-    echo "$filter: Analyzing..."
-    diec --entropy -j $directory > "/home/lk3591/Documents/datasets/Sorel/diec/$filter.txt"
+    num=$(ls $directory | wc -l)
+    echo "Scanning ($num)..." >> $logfile 2>&1
+    t_start=$(date +%s)
+    for file in $directory/*; do
+        if [[ $file != *".exe" ]]; then
+            continue
+        fi
+	outfile=$(basename "$file")
+	outfile="${outfile%.*}.txt"
+	timeout $DIEC_TIMEOUT diec --recursivescan -j $file >> $OUTPUT/recursive/$outfile
+	timeout $DIEC_TIMEOUT diec --deepscan -j $file >> $OUTPUT/deep/$outfile
+	timeout $DIEC_TIMEOUT diec --heuristicscan -j $file >> $OUTPUT/heuristic/$outfile
+    done
+    t_end=$(date +%s)
+    t_scan=$((t_end-t_start))
 
-    sleep 1
-
-    echo "$filter: Cleaning..."
-    rm -rf $directory
+    num=$(ls $directory | wc -l)
     rm $directory/* > /dev/null 2>&1
     rmdir $directory > /dev/null 2>&1
-
+    rm -rf $directory > /dev/null 2>&1
     kill -9 $aws_pid  > /dev/null 2>&1
+
+    t_end_all=$(date +%s)
+    t_all=$((t_end_all-t_start_all))
+    echo "Process,Time,Throughput:" >> $logfile 2>&1
+    echo "Downloading,$t_download,$((t_download / num))" >> $logfile 2>&1
+    echo "Extracting,$t_extract,$((t_extract / num))" >> $logfile 2>&1
+    echo "Scanning,$t_scan,$((t_downscan / num))" >> $logfile 2>&1
+    echo "All,$t_all,$((t_all / num))" >> $logfile 2>&1
+
+    echo "Finished $filter in $t_all seconds"
 }
 
 test() {
@@ -62,44 +109,61 @@ test() {
 export -f run
 export -f test
 
+MODE=$1
+JOBS=$2
+ULIMIT=$((JOBS * 8))
+
+if [ "$MODE" -eq 1 ]; then
+    TOTAL=16
+    FORMAT="%01x"
+elif [ "$MODE" -eq 2 ]; then
+    TOTAL=256
+    FORMAT="%02x"
+elif [ "$MODE" -eq 3 ]; then
+    TOTAL=4096
+    FORMAT="%03x"
+elif [ "$MODE" -eq 4 ]; then
+    TOTAL=65536
+    FORMAT="%04x"
+else
+    echo "Error: Invalid MODE"
+    exit 1
+fi
+
+currentLimit=$(ulimit -n)
+if [ $currentLimit -lt $ULIMIT ]; then
+    echo "CHANGING ULIMIT: $currentLimit"
+    ulimit -n $ULIMIT
+    currentLimit=$(ulimit -n)
+fi
+
+echo "MODE: $MODE"
+echo "JOBS: $JOBS"
+echo "FORMAT: $FORMAT"
+echo "ULIMIT: $ULIMIT"
+
 pkill aws > /dev/null 2>&1
 pkill diec > /dev/null 2>&1
 pkill zlib-flate > /dev/null 2>&1
 
-rm /home/lk3591/Documents/datasets/Sorel/diec/* > /dev/null 2>&1
-rm /home/lk3591/Documents/code/RawByteClf/logs/packing_aws_* > /dev/null 2>&1
-rm -rf /tmp/sorel/* > /dev/null 2>&1
+echo "OUTPUT: $OUTPUT"
+echo "LOG: $LOG"
+echo "DOWNLOAD: $DOWNLOAD"
 
-JOBS=1024
-ULIMIT=16384
+rm -rf $DOWNLOAD > /dev/null 2>&1
+rm -rf $OUTPUT > /dev/null 2>&1
+rm $LOG/packing_* > /dev/null 2>&1
 
-currentLimit=$(ulimit -n)
-echo "Current ULimit: $currentLimit"
-ulimit -n $ULIMIT
-currentLimit=$(ulimit -n)
-echo "New ulimit: $currentLimit"
+mkdir $DOWNLOAD
+mkdir $OUTPUT
+mkdir $OUTPUT/recursive
+mkdir $OUTPUT/deep
+mkdir $OUTPUT/heuristic
 
-# 
-# for ((i=0; i<256; i++)); do
-#     filter=$(printf "%02x" "$i")
-#     echo "$filter"
-# done | parallel --jobs $JOBS run
-
-# Takes ~10 seconds to connect plus ~100 seconds per aws batch for ~2450 files (2.1GiB)
-# for ((i=0; i<4096; i++)); do
-#     filter=$(printf "%03x" "$i")
-#     echo "$filter"
-# done | parallel --jobs $JOBS run
-
-# Takes ~10 seconds to connect plus ~10 seconds per aws batch for ~175 files (0.2GiB)
-for ((i=0; i<65536; i++)); do
-    filter=$(printf "%04x" "$i")
+for ((i=0; i<$TOTAL; i++)); do
+    filter=$(printf $FORMAT "$i")
     echo "$filter"
 done | parallel --jobs $JOBS run
-
-
-# echo "sha,status" > /home/lk3591/Documents/datasets/Sorel/packing.csv
-
 
 # for file in /home/lk3591/Documents/datasets/Sorel/diec/*.txt; do
 #     sha=$(basename "$file" | cut -d '.' -f 1)
