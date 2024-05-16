ea3e56c7883ba4b3aa99fb9fe2cfcdba7ab7b181
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-05-16 13:48:02 -0400

    Fixed printing bugs for language modeling.

--- src/learn/printers.py
+++ src/learn/printers.py
@@ -20,6 +20,7 @@ def print_tokenizer(tokenizer: PreTrainedTokenizerFast) -> None:
         "padding_side",
         "truncation_side",
         "clean_up_tokenization_spaces",
+        "model_input_names",
     ]
 
     tokens_to_ids = {k: v for k, v in zip(tokenizer.all_special_tokens, tokenizer.all_special_ids)}
@@ -27,35 +28,43 @@ def print_tokenizer(tokenizer: PreTrainedTokenizerFast) -> None:
 
     print(f"Tokenizer: {tokenizer.__class__.__name__}")
     for k in keys:
-        print(f"\t{k}: {getattr(tokenizer, k)}")
-    print(f"\tmodel_input_names: {tokenizer.model_input_names}")
+        if hasattr(tokenizer, k):
+            print(f"\t{k}: {getattr(tokenizer, k)}")
     print(f"\tspecials: {pformat(specials)}")
 
 
 def print_data_collator(data_collator: DefaultDataCollator) -> None:
-    keys = ["padding", "max_length", "pad_to_multiple_of", "return_tensors"]
-    print(f"DataCollator: {data_collator.__class__.__name__}")
+    keys = [
+        "padding",
+        "max_length",
+        "pad_to_multiple_of",
+        "return_tensors",
+        "mlm",
+        "mlm_probability",
+    ]
 
-    if data_collator.tokenizer is not None:
+    print(f"DataCollator: {data_collator.__class__.__name__}")
+    if hasattr(data_collator, "tokenizer") and data_collator.tokenizer is not None:
         print(f"\ttokenizer: {data_collator.tokenizer.__class__.__name__}")
     for k in keys:
-        print(f"\t{k}: {getattr(data_collator, k)}")
+        if hasattr(data_collator, k):
+            print(f"\t{k}: {getattr(data_collator, k)}")
 
 
 def print_config(config: PretrainedConfig) -> None:
+
+    def simplify_ids_and_labels(s: str, key: str) -> str:
+        idx = s.find(key)
+        if idx == -1:  # Config must be for langauge modeling.
+            return s
+        idx += len(key)  # index of either "{" or "N" character
+        if s[idx] == "{":  # Strip
+            idx_end = s.index("}", idx, None)  # index of closing "}"
+            s = s[:idx + 1] + "..." + s[idx_end:]
+        return s
+
     print(f"Config: {config.__class__.__name__}")
     s = pformat(config)
-
-    key = '"id2label": '
-    idx = s.index(key) + len(key)  # index of either "{" or "N" character
-    if s[idx] == "{":  # Strip
-        idx_end = s.index("}", idx, None)  # index of closing "}"
-        s = s[:idx + 1] + "..." + s[idx_end:]
-
-    key = '"label2id": '
-    idx = s.index(key) + len(key)  # index of either "{" or "N" character
-    if s[idx] == "{":  # Strip
-        idx_end = s.index("}", idx, None)  # index of closing "}"
-        s = s[:idx + 1] + "..." + s[idx_end:]
-
+    s = simplify_ids_and_labels(s, '"id2label": ')
+    s = simplify_ids_and_labels(s, '"label2id": ')
     print(s)
