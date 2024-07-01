13f0ad452a01f50152c0c4d63c702f68b2c27141
Luke Kurlandski <lk3591@g.rit.edu>
2024-07-01 10:40:39 -0400

    New files.

--- run/download_sorel.sh
+++ run/download_sorel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-find /home/lk3591/Documents/datasets/Sorel/binaries -type f -print | awk -F/ '{print $NF}' | awk -F. '{print $1}' > /home/lk3591/Documents/code/RawByteClf/tmp/sorel_exclude.txt
-for i in {0..499}; do sbatch run/download_sorel_labeled.sh $i; done
-for i in {0..999}; do sbatch run/download_sorel_unlabeled.sh $i; done
+find /shared/rc/admalware/Sorel/binaries/ -type f | awk -F/ '{print $NF}' | sed 's/\.[^.]*$//' > /home/lk3591/Documents/RawByteClf/tmp/exclude.txt
+
+for i in {0..99}; do sbatch run/_download_sorel.sh $i; done
 
