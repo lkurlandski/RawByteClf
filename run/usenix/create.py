29ae2eae93a369fdbcda0d64bd65dcd02386b027
Luke Kurlandski <lk3591@g.rit.edu>
2024-07-04 18:10:47 -0400

    Config for the (real) sample reweighting jobs.

--- run/usenix/create.py
+++ run/usenix/create.py
@@ -51,7 +51,7 @@ MODEL_NAME_AND_ARCH_CONFIGS = {
     # "mamba": '{"mode": "uni", "num_hidden_layers": 12, "hidden_size": 384, "embedding_size": 384, "mlp_hidden_size": -1}',  # 11.8 M
     # "mamba": '{"mode": "uni", "num_hidden_layers": 16, "hidden_size": 512, "embedding_size": 8, "mlp_hidden_size": -1}',  # 27.3 M
     # "mamba": '{"mode": "uni", "num_hidden_layers": 16, "hidden_size": 512, "embedding_size": 512, "mlp_hidden_size": -1}',  # 27.4 M
-    # "malconv2": '{"mode": "gcg", "channels": 256, "stride": 64, "kernel_size": 64, "embedding_size": 8}',  # 2.56 M
+    "malconv2": '{"mode": "gcg", "channels": 256, "stride": 64, "kernel_size": 64, "embedding_size": 8}',  # 2.56 M
     # "malconv2": '{"mode": "gcg", "channels": 256, "stride": 64, "kernel_size": 64, "embedding_size": 256}',  # 67.7 M
 }
 PRETRAINING_TASKS = [None, "clm-sor"]
@@ -59,7 +59,7 @@ TASKS_SCMF = ["clf-bod", "clf-sor-fam", "clf-sor-file"]
 TASKS_MCMF = ["clf-sor-class_", "clf-sor-beh", "clf-sor-pack"]
 TASKS = TASKS_SCMF + TASKS_MCMF
 MIN_FREQ = [None, 100]
-TR_SAMPLES_PER_CLASS = [None, 1, 5]
+TR_SAMPLES_PER_CLASS = [None,]# 1, 5]
 SEEDS = [0, 1, 2]
 
 
@@ -210,7 +210,7 @@ def get_body_clf(
 
     --task='{downstream_task}' \\
     --pretraining_task='{pretraining_task}' \\
-    --pretraining_checkpoint=0 \\
+    --pretraining_checkpoint=-1 \\
     --tr_size=0.85 \\
     --vl_size=0.15 \\
     --ts_size=0.0 \\
@@ -269,7 +269,7 @@ def get_jobname(
         str(tr_samples_per_class),
         str(seed),
     ]
-    return "chck0--" + "--".join(args)
+    return "--".join(args)
 
 
 def compute_mem(
@@ -438,10 +438,6 @@ def main():
 
         # Classification
         for task in TASKS:
-            weighted_loss = "sample_reweighting" if task in TASKS_SCMF else None
-
-            # FIXME: due to a (stupid) bug, nothing has actually been run with the weighted loss.
-            weighted_loss = None
 
             for pretraining_task in PRETRAINING_TASKS:
                 if model_name in ("malconv", "malconv2") and pretraining_task is not None:
@@ -450,6 +446,8 @@ def main():
                 for tr_samples_per_class in TR_SAMPLES_PER_CLASS:
                     if tr_samples_per_class is not None and task not in TASKS_SCMF:
                         continue
+
+                    weighted_loss = "sample_reweighting" if (task in TASKS_SCMF and tr_samples_per_class is None) else None
 
                     for min_freq in MIN_FREQ:
                         if min_freq is not None and tr_samples_per_class is not None:
