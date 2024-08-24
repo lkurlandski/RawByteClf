3568bd53f696e2570aa1401fe6077030ae437bd5
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-08-24 11:07:31 -0400

    Refactored to run based on triplet slicing.

--- src/data/lift.sh
+++ src/data/lift.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 
+#
+#
+#
+
+
 export SCRIPTS="/home/lk3591/Documents/code/RawByteClf/ghidra_scripts"
 
 
-run() {
+run_ghidra() {
 
-  location="$ROOT/ghidra/$1"
-  input="$ROOT/binaries/$1"
-  disassembled="$ROOT/disassembled/$1"
-  decompiled="$ROOT/decompiled/$1"
+  # Run Ghidra recursively.
+ 
+  # Parameters:
+    # $1 (location)
+    # $2 (input)
+    # $3 (disassembled)
+    # $4 (decompiled)
 
-  mkdir -p "$location"
-  mkdir -p "$disassembled"
-  mkdir -p "$decompiled"
+  local location=$1
+  local input=$2
+  local disassembled=$3
+  local decompiled=$4
+
+  mkdir -p $location
+  mkdir -p $disassembled
+  mkdir -p $decompiled
 
   analyzeHeadless \
     "$location" "lift" \
@@ -24,6 +37,48 @@ run() {
     -postScript "disassembler.py" "$disassembled" \
     -postScript "decompiler.py" "$decompiled"
 
+}
+export -f run_ghidra
+
+
+run() {
+
+  # Lift binaries that begin with `hhh`.
+
+  # Parameters:
+    # $1 (hhh)
+
+  local hhh=$1
+  local hh="${hhh:0:2}"
+
+  local location="$ROOT/ghidra/$hhh"  # Must be a unique directory.
+  local disassembled="$ROOT/disassembled/$hh"
+  local decompiled="$ROOT/decompiled/$hh"
+
+  local source="$ROOT/binariesTmp/$hh" # FIXME: change before committing.
+  local input="$ROOT/.liftTmp/$hhh"
+  mkdir -p "$input"
+
+  for file in "$source"/"$hhh"*; do
+    if [ -e "$file" ]; then
+      filename=$(basename "$file")
+        ln -s "$file" "$input/$filename"
+    fi
+  done
+
+  # FIXME: remove
+  echo "hhh: --$hhh--"
+  echo "hh: --$hh--"
+  echo "location: --$location--"
+  echo "disassembled: --$disassembled--"
+  echo "decompiled: --$decompiled--"
+  echo "source: --$source--"
+  echo "input: --$input--"
+  # exit
+
+  run_ghidra "$location" "$input" "$disassembled" "$decompiled"
+
+  rm -rf "$input" 
 }
 export -f run
 
@@ -47,12 +102,15 @@ echo "ROOT: $ROOT"
 echo "TIMEOUT: $TIMEOUT"
 echo "JOBS: $JOBS"
 
-# rm -rf "logs/lift_"*
-# rm -rf "$ROOT/ghidra/"*
-# rm -rf "$ROOT/disassembled/"*
-# rm -rf "$ROOT/decompiled/"*
+rm -rf "logs/lift_"*
+rm -rf "$ROOT/.liftTmp/"
+rm -rf "$ROOT/ghidra/"
+rm -rf "$ROOT/disassembled/"
+rm -rf "$ROOT/decompiled/"
 
-parallel --bar -j $JOBS \
-  'run {1} > ./logs/lift_{1}.txt 2>&1' \
-  ::: $(printf "%02x\n" {0..255})
+run "fff"
+
+# parallel --bar -j $JOBS \
+#    'run {1} > ./logs/lift_{1}.txt 2>&1' \
+#    ::: $(printf "%02x\n" {255..0})
 
