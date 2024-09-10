a5667039647900acf8e689bddf45cced76c9654c
Luke Kurlandski <lk3591@g.rit.edu>
2024-09-10 12:44:49 -0400

    Very close to a final version of the decompiler.

--- ghidra_scripts/Decompiler.java
+++ ghidra_scripts/Decompiler.java
@@ -1,25 +1,56 @@
-// Decompiler.java
+/*
+ * Decompile binaries.
+*/
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
 import ghidra.app.decompiler.*;
 import ghidra.app.script.GhidraScript;
 import ghidra.program.model.listing.*;
 import ghidra.program.model.symbol.*;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
 
 public class Decompiler extends GhidraScript {
 
+    private static final boolean SKIP_PARAMETERS_WITH_UNKNOWN_TYPE = false;
+    private static final boolean SKIP_EXTERNAL_FUNCTIONS = false;
+    private static final boolean FORMAL_SIGNATURE = false;
+    private static final boolean INCLUDE_CALLING_CONVENTION = true;
+
+    private int timeoutPerFile = -1;
+    private int timeoutPerFunc = -1;
+
     @Override
     protected void run() throws Exception {
+	// Log configuration.
+        println("run: SKIP_PARAMETERS_WITH_UNKNOWN_TYPE=" + String.valueOf(SKIP_PARAMETERS_WITH_UNKNOWN_TYPE));
+        println("run: SKIP_EXTERNAL_FUNCTIONS=" + String.valueOf(SKIP_EXTERNAL_FUNCTIONS));
+        println("run: FORMAL_SIGNATURE=" + String.valueOf(FORMAL_SIGNATURE));
+        println("run: INCLUDE_CALLING_CONVENTION=" + String.valueOf(INCLUDE_CALLING_CONVENTION));
 
         // Get the command line arguments.
         String[] scriptArgs = getScriptArgs();
-        if (scriptArgs == null || scriptArgs.length < 1) {
-            println("Error: input directory and timeout required.");
+        if (scriptArgs == null || scriptArgs.length < 3) {
+            println("Error: outputDir, timeoutPerFile, timeoutPerFunc required.");
             return;
         }
-
-        int timeout = Integer.parseInt(scriptArgs[1]);
+        String outputDir = scriptArgs[0];
+        this.timeoutPerFile = Integer.parseInt(scriptArgs[1]);
+        this.timeoutPerFunc = Integer.parseInt(scriptArgs[2]);
+	println("run: outputDir=" + outputDir);
+	println("run: timeoutPerFile=" + String.valueOf(this.timeoutPerFile));
+        println("run: timeoutPerFunc=" + String.valueOf(this.timeoutPerFunc));
 
         // Get the current program.
         Program program = getCurrentProgram();
@@ -31,80 +62,101 @@ public class Decompiler extends GhidraScript {
         if (programName.contains(".")) {
             programName = programName.substring(0, programName.lastIndexOf('.'));
         }
+	println("run: programName=" + programName);
 
-        // Determine the file to write to.
-        String outputDir = scriptArgs[0];
+        // Get the output file.
         File dir = new File(outputDir);
         if (!dir.exists()) {
             println("Error: output directory does not exist.");
             return;
         }
         String outputFileName = outputDir + File.separator + programName + ".c";
+	println("run: outputFileName=" + outputFileName);
+
+        // Get the functions to process.
+	FunctionIterator functions;
+	if (SKIP_EXTERNAL_FUNCTIONS) {
+	    functions = program.getFunctionManager().getFunctions(true);
+	} else {
+	    functions = program.getListing().getFunctions(true);
+	}
 
         // Set up the DecompilerInterface.
         DecompInterface decompInterface = new DecompInterface();
         decompInterface.openProgram(program);
 
-        // Write each function to the output file.
-        FunctionIterator functions = program.getFunctionManager().getFunctions(true);
-        try (FileWriter writer = new FileWriter(outputFileName)) {
+        // Run the main worker function.
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Callable<Void> task = () -> {
+            decompileFunctions(decompInterface, functions, outputFileName);
+            return null;
+        };
+        Future<Void> future = executor.submit(task);
+        try {
+            future.get(this.timeoutPerFile, TimeUnit.SECONDS);
+            println("run: decompilation finished.");
+        } catch (TimeoutException e) {
+            println("run: decompilation timed out.");
+            future.cancel(true);
+        } catch (InterruptedException | ExecutionException e) {
+            println("run: decompilation crashed.");
+            e.printStackTrace();
+        } finally {
+            decompInterface.dispose();
+            executor.shutdown();
+        }
+    }
+
+    private void decompileFunctions(DecompInterface decompInterface, FunctionIterator functions, String outputFileName) {
+        String decompiledCode;
+	try (FileWriter writer = new FileWriter(outputFileName)) {
             for (Function func : functions) {
-                decompileFunction(decompInterface, func, timeout, writer);
+                decompiledCode = decompileFunction(decompInterface, func);
+		writer.write(decompiledCode);
             }
         } catch (IOException e) {
             println("Failed to write output file: " + e.getMessage());
         }
-
-        // Close the interface.
-        decompInterface.dispose();
     }
 
-    private void decompileFunction(DecompInterface decompInterface, Function func, int timeout, FileWriter writer) {
-        try {
-            DecompileResults results = decompInterface.decompileFunction(func, timeout, monitor);
+    private String decompileFunction(DecompInterface decompInterface, Function func) {
+
+        DecompileResults results = decompInterface.decompileFunction(func, this.timeoutPerFunc, null);
+        String signature = func.getPrototypeString(FORMAL_SIGNATURE, INCLUDE_CALLING_CONVENTION);
+
+	// If decompilation was successful, we get the C code, then replace the signature
+	// with the more detailed signature from Function.getPrototypeString. The default
+	// signature from DecompiledFunction.getC can also be very inconsitent when it
+	// chooses to include and exclude call conventions, is unclear.
+	if (results.decompileCompleted()) {
             DecompiledFunction decompiledFunc = results.getDecompiledFunction();
-
-            if (decompiledFunc == null) {
-                // Get the function signature
-                String functionSignature = func.getPrototypeString(true, false);
-        
-                // Write the function signature along with the timeout warning
-                writer.write(functionSignature + "\n{\n\n");
-                writer.write("/* WARNING: Could not decompile function within timeout */\n");
-                writer.write("}\n\n");
-
-                println("Failed to decompile function: " + func.getName() + " within timeout.");
-                return;
-            }
-
-            // Write the decompiled function code
             String decompiledCode = decompiledFunc.getC();
-            writer.write(decompiledCode);
+	    String[] decompiledCodeLines = decompiledCode.split("\n");
+            decompiledCodeLines[1] = signature;
+	    decompiledCode = String.join("\n", decompiledCodeLines);
+            return decompiledCode;
+	}
 
-        } catch (IOException e) {
-            println("Failed to write decompiled function: " + e.getMessage());
-        }
+	// If decomilation timed out, was cancelled, or failed for some other reason,
+	// return the function signature along with a comment documenting the failure.
+        String message;
+	if (results.isTimedOut()) {  // NOTE: this doesn't seem to trigger correctly.
+	    println("decompileFunction: results.isTimedOut()=true");
+            message = "WARNING: Decompilation incomplete due to timeoutPerFile="
+		    + String.valueOf(this.timeoutPerFile) + ".";
+	} else if (results.isCancelled()) {  // NOTE: this doesn't seem to trigger correctly.
+	    println("decompileFunction: results.isCancelled()=true");
+	    message = "WARNING: Decompilation incomplete due to timeoutPerFunc="
+		    + String.valueOf(this.timeoutPerFunc) + ".";
+	} else {
+	    println("decompileFunction: results.getErrorMessage()=" + results.getErrorMessage().replace("\n", ""));
+	    message = "WARNING: Decompilation incomplete due to ErrorMessage=`"
+		    + results.getErrorMessage().replace("\n", "") + "`.";
+	}
+
+	return signature + "\n{\n\n/* " + message + " */\n}\n\n";
+
     }
 
-    /*
-    private void decompileFunction(DecompInterface decompInterface, Function func, int timeout, FileWriter writer) {
-        try {
-
-            DecompileResults results = decompInterface.decompileFunction(func, timeout, monitor);
-            DecompiledFunction decompiledFunc = results.getDecompiledFunction();
-
-            if (decompiledFunc == null) {
-                println("Failed to decompile function: " + func.getName());
-                return;
-            }
-
-            String decompiledCode = decompiledFunc.getC();
-            writer.write(decompiledCode);
-
-        } catch (IOException e) {
-            println("Failed to write decompiled function: " + e.getMessage());
-        }
-    }
-    */
 }
 
