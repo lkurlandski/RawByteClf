cda6adf9c0d4a84d4fa8445b7aed080eaeda1623
lkurlandski <lk3591@g.rit.edu>
2024-09-27 11:15:38 -0400

    Preprocessing for raw data.

--- src/data/prepare_data_for_esp.py
+++ src/data/prepare_data_for_esp.py
@@ -3,8 +3,11 @@ Prepare raw, disassembled, and decompiled data caches. This is essentially norma
 """
 
 from argparse import ArgumentParser
+from functools import partial
+import json
 import multiprocessing as mp
 from pathlib import Path
+from pprint import pformat
 import re
 import time
 from typing import Callable
@@ -14,7 +17,6 @@ from tqdm import tqdm
 from unidecode import unidecode
 
 
-NUM_WORKERS = 16
 ROOT = Path("/media/lk3591/easystore/datasets")
 ROOTS = {
     "sorel_pe": ROOT / "Sorel",
@@ -24,12 +26,14 @@ ROOTS = {
 IN = "ghidra"
 OUT = "processed"
 
+NUM_WORKERS: int = None
 
-def _run(f: Path, f_out: Path, func: Callable[[bytes, str], bytes]) -> tuple[int, int]:
+
+def _run(f: Path, f_out: Path, func: Callable[[bytes, str], bytes], disable_tqdm: bool = True) -> tuple[int, int]:
     s_org = 0
     s_new = 0
     with ZipFile(f, "r") as zp, ZipFile(f_out, "w") as zp_out:
-        for n in zp.namelist():
+        for n in tqdm(zp.namelist(), leave=False, disable=disable_tqdm):
             b = zp.read(n)
             b_out = func(b, n.split(".")[0])
             zp_out.writestr(n, b_out, ZIP_DEFLATED, 9)
@@ -44,8 +48,12 @@ def run(path: Path, out: Path, func: Callable[[bytes, str], bytes]) -> None:
     files = sorted(path.rglob("*.zip"))
     outfiles = [out / f.name for f in files]
 
-    with mp.Pool(NUM_WORKERS) as pool:
-        info = pool.starmap(_run, list(zip(files, outfiles, [func] * len(files))))
+
+    if NUM_WORKERS is not None and NUM_WORKERS > 1:
+        with mp.Pool(NUM_WORKERS) as pool:
+            info = pool.starmap(_run, list(zip(files, outfiles, [func] * len(files))))
+    else:
+        info = [_run(f, f_out, func, disable_tqdm=False) for f, f_out in tqdm(list(zip(files, outfiles)))]
 
     for f, f_out, (s_org, s_new) in zip(files, outfiles, info):
         print(f"Processed {f} -> {f_out} ({s_org} -> {s_new})")
@@ -54,9 +62,15 @@ def run(path: Path, out: Path, func: Callable[[bytes, str], bytes]) -> None:
     print(f"Total Delta: {delta / 1e9:.1f}GB")
 
 
-d: dict[str, list[tuple[int, int]]] = dict()  # TODO
-def raw_func(b: bytes, n: str) -> bytes:
-    return b"".join([b[l:u] for l, u in d[n]])
+def raw_func(b: bytes, n: str, d: dict[str, list[tuple[int, int]]]) -> bytes:
+    if n in d:
+        bounds = d[n]
+    elif n + ".exe" in d:
+        bounds = d[n + ".exe"]
+    else:
+        raise KeyError(f"Could not find bounds for {n}")
+    b_new =  b"".join([b[l:u] for l, u in bounds])
+    return b_new
 
 def raw(dataset: str) -> None:
 
@@ -64,7 +78,12 @@ def raw(dataset: str) -> None:
     out = ROOTS[dataset] / OUT / "raw"
     out.mkdir(parents=True, exist_ok=True)
 
-    run(path, out, raw_func)
+    f = ROOTS[dataset] / "executableSections.json"
+    with open(f, "r") as fp:
+        d = json.load(fp)
+    d = {name: data["bounds"] for name, data in d.items()}
+
+    run(path, out, partial(raw_func, d=d))
 
 
 def dis_func(b: bytes, n: str) -> bytes:  # pylint: disable=unused-argument
@@ -108,7 +127,13 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("--dataset", type=str, choices=["sorel_pe", "bodmas_pe", "assemblage_pe"], required=True)
     parser.add_argument("--lift_level", type=str, choices=["raw", "dis", "dec"], required=True)
+    parser.add_argument("--num_workers", type=int, default=None)
     args = parser.parse_args()
+
+    print(f"args={pformat(args.__dict__)}")
+
+    global NUM_WORKERS
+    NUM_WORKERS = args.num_workers
 
     t_i = time.time()
 
