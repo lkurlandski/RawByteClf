2e84bf18c7b846892fa0b54fbea734e4df85f0fa
Luke Kurlandski <lk3591@g.rit.edu>
2024-10-07 21:02:44 -0400

    Full fuzz script.

--- run/fuzz_esp_loader_endpoints.sh
+++ run/fuzz_esp_loader_endpoints.sh
@@ -2,21 +2,20 @@
 
 #SBATCH --job-name=fuzz
 #SBATCH --account=admalware
-#SBATCH --partition=debug
+#SBATCH --partition=tier3
 #SBATCH --output=./logs/%x_%j.out
-#SBATCH --time=00-01:00:00
+#SBATCH --time=01-12:00:00
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=32
 #SBATCH --ntasks=1
-#SBATCH --mem=32G
+#SBATCH --mem=256G
 
 
 # Each worker needs ~8G
-# 32 workers = 256G
 
 
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate RawByteClf2
 
 
-python -u src/data/fuzz_esp_loader_endpoints.py --task=det --num_workers=1 --subset=16
+python -u src/data/fuzz_esp_loader_endpoints.py --task="det" --num_workers=32
