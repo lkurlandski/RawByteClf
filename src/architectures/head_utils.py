c4daa17684c0becbb462c3e41d3f56e99a40ad6e
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2024-10-15 13:08:23 -0400

    Nicely flushed out head utilities.

---
+++ src/architectures/head_utils.py
@@ -0,0 +1,155 @@
+"""
+Utilities for the classification and language modeling heads.
+
+TODO:
+ - figure out how to tie weights for language modeling tasks.
+"""
+
+from typing import Optional
+
+import torch
+from torch import nn, Tensor
+
+
+class ShapeError(ValueError):
+
+    def __init__(self, actual_shape: tuple, expected_shape: Optional[tuple] = None):
+        self.expected_shape = tuple(expected_shape)
+        self.actual_shape = tuple(actual_shape) if actual_shape else None
+        super().__init__(f"Recieved: {self.actual_shape}. Expected: {self.expected_shape}")
+
+
+class Head(nn.Module):
+
+    """
+    A generic head with one of two structures:
+
+        Linear(in_size, out_size)
+
+        Linear(in_size, hidden_size)
+        ReLU()
+        Dropout(dropout_rate)
+        ...
+        Linear(hidden_size, out_size)
+    """
+
+    def __init__(
+        self,
+        in_size: int,
+        out_size: int,
+        hidden_size: int = -1,
+        num_hidden_layers: int = 0,
+        dropout_rate: float = 0.1,
+    ) -> None:
+        super().__init__()
+
+        self.layers = nn.ModuleList()
+
+        if num_hidden_layers == 0:
+            self.layers.append(nn.Linear(in_size, out_size))
+            return
+
+        self.layers.append(nn.Linear(in_size, hidden_size, dropout_rate))
+        for _ in range(1, num_hidden_layers):
+            self.add_layer(hidden_size, hidden_size, dropout_rate)
+        self.layers.append(nn.Linear(hidden_size, out_size))
+
+    def add_layer(self, in_size: int, out_size: int, dropout_rate: float) -> list[nn.Module]:
+        self.layers.append(nn.Linear(in_size, out_size))
+        self.layers.append(nn.ReLU())
+        self.layers.append(nn.Dropout(dropout_rate))
+
+    def forward(self, last_hidden_state: Tensor) -> Tensor:
+        if last_hidden_state.dim() != 3:
+            raise ShapeError(last_hidden_state.shape, ("B", "T", "H"))
+
+        x = last_hidden_state
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+def pool_logits(pooling: str, logits: Tensor, input_ids: Tensor, pad_token_id: int) -> Tensor:
+    if logits.dim() != 3:
+        raise ShapeError(logits.shape, ("B", "T", "H"))
+
+    if pooling == "none":
+        return logits
+
+    if pooling == "mean":
+        pooled_logits = torch.mean(logits, dim=1)
+        return pooled_logits
+
+    if pooling == "last":
+        batch_size = logits.shape[0]
+        sequence_lengths = torch.eq(input_ids, pad_token_id).int().argmax(-1) - 1
+        sequence_lengths = sequence_lengths % input_ids.shape[-1]
+        sequence_lengths = sequence_lengths.to(logits.device)
+        pooled_logits = logits[torch.arange(batch_size, device=logits.device), sequence_lengths]
+        return pooled_logits
+
+    raise ValueError(f"Invalid pooling: {pooling}")
+
+
+def get_clm_loss(logits: Tensor, labels: Tensor, num_labels: int) -> Tensor:
+    if logits.dim() != 3:
+        raise ShapeError(logits.shape, ("B", "T", "V"))
+    if labels.dim() != 2:
+        raise ShapeError(labels.shape, ("B", "T"))
+
+    shifted_logits = logits[:, :-1, :].contiguous()
+    shifted_labels = labels[:, 1:].contiguous()
+    loss_fct = nn.CrossEntropyLoss()
+    loss = loss_fct(shifted_logits.view(-1, num_labels), shifted_labels.view(-1))
+    return loss
+
+
+def get_mlm_loss(logits: Tensor, labels: Tensor, num_labels: int) -> Tensor:
+    if logits.dim() != 3:
+        raise ShapeError(logits.shape, ("B", "T", "V"))
+    if labels.dim() != 2:
+        raise ShapeError(labels.shape, ("B", "T"))
+
+    loss_fct = nn.CrossEntropyLoss()
+    loss = loss_fct(logits.view(-1, num_labels), labels.view(-1))
+    return loss
+
+
+def get_clf_loss(logits: Tensor, labels: Tensor, num_labels: int, problem_type: str, ) -> Tensor:
+
+    if problem_type == "regression":
+        if logits.dim() != 1:
+            raise ShapeError(logits.shape, ("B",))
+        if labels.dim() != 1:
+            raise ShapeError(labels.shape, ("B",))
+
+        loss_fct = nn.MSELoss()
+        if num_labels == 1:
+            loss = loss_fct(logits.squeeze(), labels.squeeze())
+            return loss
+        loss = loss_fct(logits, labels)
+        return loss
+
+    if problem_type == "single_label_classification":
+        if logits.dim() != 2:
+            raise ShapeError(logits.shape, ("B", "C"))
+        if labels.dim() != 1:
+            raise ShapeError(labels.shape, ("B",))
+
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(logits.view(-1, num_labels), labels.view(-1))
+        return loss
+
+    if problem_type == "multi_label_classification":
+        if logits.dim() != 2:
+            raise ShapeError(logits.shape, ("B", "C"))
+        if labels.dim() != 2:
+            raise ShapeError(labels.shape, ("B", "C"))
+        if not torch.is_floating_point(labels):
+            raise TypeError(f"Expected labels to be a floating point, got {labels.dtype}")
+
+        loss_fct = nn.BCEWithLogitsLoss()
+        loss = loss_fct(logits, labels)
+        return loss
+
+    raise ValueError(f"Invalid problem type: {problem_type}")
--- tests/test_architectures.py
+++ tests/test_architectures.py
@@ -9,6 +9,7 @@ import sys
 import unittest
 
 import torch
+from torch import nn
 from tqdm import tqdm
 
 if __name__ == "__main__":
@@ -16,6 +17,7 @@ if __name__ == "__main__":
 
 from src.architectures.malconv_hf import MalConvForSequenceClassification, MalConvConfig
 from src.architectures.malconv2 import MalConv2ForSequenceClassification, MalConv2Config
+from src.architectures.head_utils import Head, pool_logits, get_clm_loss, get_mlm_loss, get_clf_loss
 
 
 class TestMalConvForSequenceClassification(unittest.TestCase):
@@ -194,6 +196,78 @@ class TestMalConv2ForSequenceClassification(unittest.TestCase):
 
     def test_weird_gcg(self):
         self._test_model(self.config_weird_gcg)
+
+
+class TestHeadUtils(unittest.TestCase):
+
+    B = 4       # Batch size
+    H = 64      # Hidden size
+    T = 1024    # Sequence length
+    C = 10      # Number of classes
+    L = 2       # Number of hidden layers
+    V = 256     # Vocabulary size
+    P = 0       # Pad token
+
+    def test_head_simple(self):
+        model = Head(in_size=self.H, out_size=self.C)
+        input_tensor = torch.randn(self.B, self.T, self.H)
+        output = model(input_tensor)
+        self.assertEqual(output.shape, (self.B, self.T, self.C))
+
+    def test_head_hidden(self):
+        model = Head(in_size=self.H, out_size=self.C, hidden_size=self.H, num_hidden_layers=self.L)
+        input_tensor = torch.randn(self.B, self.T, self.H)
+        output = model(input_tensor)
+        self.assertEqual(output.shape, (self.B, self.T, self.C))
+
+    def test_pool_logits_none(self):
+        logits = torch.randn(self.B, self.T, self.C)
+        input_ids = torch.randint(0, self.V, (self.B, self.T))
+        output = pool_logits("none", logits, input_ids, self.P)
+        self.assertEqual(output.shape, (self.B, self.T, self.C))
+        self.assertEqual(torch.equal(output, logits), True)
+
+    def test_pool_logits_mean(self):
+        logits = torch.randn(self.B, self.T, self.C)
+        input_ids = torch.randint(0, self.V, (self.B, self.T))
+        output = pool_logits("mean", logits, input_ids, self.P)
+        self.assertEqual(output.shape, (self.B, self.C))
+
+    def test_pool_logits_last(self):
+        logits = torch.randn(self.B, self.T, self.C)
+        input_ids = torch.randint(0, self.V, (self.B, self.T))
+        output = pool_logits("last", logits, input_ids, self.P)
+        self.assertEqual(output.shape, (self.B, self.C))
+
+    def test_get_clm_loss(self):
+        logits = torch.randn(self.B, self.T, self.V)
+        labels = torch.randint(0, self.V, (self.B, self.T))
+        loss = get_clm_loss(logits, labels, self.V)
+        self.assertTrue(isinstance(loss, torch.Tensor))
+
+    def test_get_mlm_loss(self):
+        logits = torch.randn(self.B, self.T, self.V)
+        labels = torch.randint(0, self.V, (self.B, self.T))
+        loss = get_mlm_loss(logits, labels, self.V)
+        self.assertTrue(isinstance(loss, torch.Tensor))
+
+    def test_get_clf_loss_regression(self):
+        logits = torch.randn(self.B)
+        labels = torch.randn(self.B)
+        loss = get_clf_loss(logits, labels, num_labels=1, problem_type="regression")
+        self.assertTrue(isinstance(loss, torch.Tensor))
+
+    def test_get_clf_loss_single_label_classification(self):
+        logits = torch.randn(self.B, self.C)
+        labels = torch.randint(0, self.C, (self.B,))
+        loss = get_clf_loss(logits, labels, num_labels=self.C, problem_type="single_label_classification")
+        self.assertTrue(isinstance(loss, torch.Tensor))
+
+    def test_get_clf_loss_multi_label_classification(self):
+        logits = torch.randn(self.B, self.C)
+        labels = torch.randint(0, 2, (self.B, self.C), dtype=torch.float32)
+        loss = get_clf_loss(logits, labels, num_labels=self.C, problem_type="multi_label_classification")
+        self.assertTrue(isinstance(loss, torch.Tensor))
 
 
 if __name__ == "__main__":
