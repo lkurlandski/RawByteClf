3bcc5f2ee4f54d7aedec29f899442ddce5cbd784
Luke Kurlandski <lk3591@g.rit.edu>
2024-12-13 13:13:48 -0500

    Made the multilabel task easier.

--- src/data/loaders_core.py
+++ src/data/loaders_core.py
@@ -2142,7 +2142,7 @@ def get_materials_esp_beh(
     ts_size: float = 0.00,
     lift_level_ddp: LiftLevel = LiftLevel.DECOMPILED,
     min_freq: Optional[int] = 100,
-    max_imbalance_ratio: Optional[int] = 50,
+    max_imbalance_ratio: Optional[int] = 10,
     purge_empty_samples: bool = True,
     verbose: bool = True,
 ) -> Materials:
