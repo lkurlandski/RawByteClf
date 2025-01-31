7a02d8f4201f58d144b9dfd4f646d4fba8af66d5
Luke Kurlandski <lk3591@g.rit.edu>
2025-01-31 12:23:49 -0500

    Enhanced the tests for label consistency.

--- src/data/check_esp_loader_caches.py
+++ src/data/check_esp_loader_caches.py
@@ -79,13 +79,43 @@ for get_materials in GET_MATERIALS:
             multifiles[lift_level_2]["tr"] | multifiles[lift_level_2]["vl"])
         if s_1:
             errors_detected = True
-            print(f"Warning: dissimilar files detected! ({get_materials.__name__} {lift_level.value} tr {len(s_1)})")
+            print(f"Warning: dissimilar files detected! ({get_materials.__name__} {lift_level_1.value} {lift_level_2.value} tr {len(s_1)})")
         if s_2:
             errors_detected = True
-            print(f"Warning: dissimilar files detected! ({get_materials.__name__} {lift_level.value} vl {len(s_2)})")
+            print(f"Warning: dissimilar files detected! ({get_materials.__name__} {lift_level_1.value} {lift_level_2.value} vl {len(s_2)})")
         if s_3:
             errors_detected = True
-            print(f"Warning: dissimilar files detected! ({get_materials.__name__} {lift_level.value} al {len(s_3)})")
+            print(f"Warning: dissimilar files detected! ({get_materials.__name__} {lift_level_1.value} {lift_level_2.value} al {len(s_3)})")
+
+        if get_materials in (get_materials_esp_clm, get_materials_esp_mlm):
+            continue
+
+        for s in ["tr", "vl"]:
+            materials_1 = multimaterials[lift_level_1]
+            materials_2 = multimaterials[lift_level_2]
+            iterable_1 = zip(
+                [af.name.split(".")[0] for af in materials_1.files[s]],
+                list(materials_1.labels[s]),
+                strict=True,
+            )
+            iterable_2 = zip(
+                [af.name.split(".")[0] for af in materials_2.files[s]],
+                list(materials_2.labels[s]),
+                strict=True,
+            )
+            names_mismatch = 0
+            label_mismatch = 0
+            for (n_1, l_1), (n_2, l_2) in zip(iterable_1, iterable_2, strict=True):
+                if n_1 != n_2:
+                    if names_mismatch == 0:
+                        print(f"Warning: dissimilar files detected! ({get_materials.__name__} {lift_level_1.value} {lift_level_2.value} {s} {n_1} != {n_2}")
+                    names_mismatch += 1
+                if l_1 != l_2:
+                    if label_mismatch == 0:
+                        print(f"Warning: dissimilar labels detected! ({get_materials.__name__} {lift_level_1.value} {lift_level_2.value} {s} {l_1} != {l_2}")
+                    label_mismatch += 1
+            errors_detected = errors_detected or names_mismatch > 0 or label_mismatch > 0
+
 
     if not errors_detected:
         print(f"Success: no problems were detected! ({get_materials.__name__})")
