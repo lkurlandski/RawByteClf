0c9de34a80825b1b3ffc1697f2fa05d62c06be2b
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2025-05-01 12:21:38 -0400

    Added a function to check the correctedness of a rank matrix.

--- src/attribute/utils.py
+++ src/attribute/utils.py
@@ -5,6 +5,16 @@ Tools for performing input attribution with captum.
 from functools import wraps
 import warnings
 
+import numpy as np
+from scipy import stats
+
+
+def is_proper_rank_matrix(R: np.ndarray, *, method: str = "average", tol: float = 1e-12) -> bool:
+    for col in R.T:
+        if not np.allclose(col, stats.rankdata(col, method=method), atol=tol, rtol=0):
+            return False
+    return True
+
 
 def ignore_warnings_decorator(*filter_args, **filter_kwargs):
 
