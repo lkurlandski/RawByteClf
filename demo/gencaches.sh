ad0dd24fa576d0ebc7200ae100c745a43be10b08
Luke Kurlandski <lk3591@csec-armitage.main.ad.rit.edu>
2025-07-16 09:15:06 -0400

    Updated gencaches and tokenize.

--- demo/gencaches.sh
+++ demo/gencaches.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -l
 
+echo "gencaches.sh: Determining the samples to use for experiments and caching the results."
+
 mkdir cache/materials
 
 for lift_level in "raw" "dis" "dec"; do
     for task in "det" "beh" "fam" "mlm" "clm"; do
         for lift_level_ddp in "dec"; do
-            echo "---------------------------------------------------------------------------------------------------------"
-            echo "Starting task='${task}', lift_level='${lift_level}', lift_level_ddp='${lift_level_ddp}'"
-            python -u src/data/generate_esp_loader_caches.py \
-                    --task="${task}" \
-                    --lift_level="${lift_level}" \
-                    --lift_level_ddp="${lift_level_ddp}" > /dev/null 2>&1
-            echo "Finished task='${task}', lift_level='${lift_level}', lift_level_ddp='${lift_level_ddp}' with exit code $?"
-            echo "---------------------------------------------------------------------------------------------------------"
+            echo "Starting '${lift_level}' '${task}' '${lift_level_ddp}'"
+            python src/data/generate_esp_loader_caches.py \
+                --task="${task}" \
+                --lift_level="${lift_level}" \
+                --lift_level_ddp="${lift_level_ddp}" > /dev/null 2>&1
+            echo "Finished '${lift_level}' '${task}' '${lift_level_ddp}'. Exit code $?."
         done
     done
 done
--- demo/tokenize.sh
+++ demo/tokenize.sh
@@ -1,28 +1,20 @@
+#!/bin/bash -l
+
+echo "tokenize.sh: Training tokenizers and saving the learned vocabularies."
+
 for l in raw dis dec; do
-
     for a in bpe uni; do
-        
         for v in 1024 4096 16384; do
-
-            logfile="./logs/tok-$l-$a-$v.log"
-
-            echo "Running $l-$a-$v..."
-
-            # systemd-run --scope -p MemoryLimit=96G /home/lk3591/anaconda3/envs/RawByteClf/bin/python \
-            python \
-                src/tokenization/train.py \
+            echo "Starting '${l}' '${a}' '${v}'"
+            python src/tokenization/train.py \
                 --lift_level "$l" \
                 --algorithm "$a" \
                 --vocab_size "$v" \
                 --num_files=100 \
                 --batch_size=1024 \
                 --block_size=1024 \
-                --max_token_length=16 # &> "$logfile"
-
-            # echo "Done. Status: $?"
-
+                --max_token_length=16 > /dev/null 2>&1
+            echo "Finished '${l}' '${a}' '${v}'. Exit code $?."
         done
-
     done
-
 done
