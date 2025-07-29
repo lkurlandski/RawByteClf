588cf0b4ad0691f9a67f08e104a40757c506912c
Luke Kurlandski <lk3591@g.rit.edu>
2025-07-29 15:04:42 -0400

    check-deps.sh redirect stderr.

--- check-deps.sh
+++ check-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Checking pytorch..."
-python -c "import torch" > /dev/null
+python -c "import torch" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Success."
 else
@@ -9,7 +9,7 @@ else
 fi
 
 echo "Checking pytorch-CUDA..."
-python -c "import torch; assert torch.cuda.is_available()" > /dev/null
+python -c "import torch; assert torch.cuda.is_available()" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Success."
 else
@@ -17,7 +17,7 @@ else
 fi
 
 echo "Checking NVIDIA's CUDA Compiler (NVCC)..."
-nvcc --version > /dev/null
+nvcc --version > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Success."
 else
@@ -25,7 +25,7 @@ else
 fi
 
 echo "Checking causal_conv1d..."
-python -c "from causal_conv1d import causal_conv1d_fn, causal_conv1d_update"
+python -c "from causal_conv1d import causal_conv1d_fn, causal_conv1d_update" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Success."
 else
@@ -33,7 +33,7 @@ else
 fi
 
 echo "Checking mamba_ssm..."
-python -c "from mamba_ssm.ops.selective_scan_interface import mamba_inner_fn, selective_scan_fn"
+python -c "from mamba_ssm.ops.selective_scan_interface import mamba_inner_fn, selective_scan_fn" > /dev/null 2>&1
 if [ $? -eq 0 ]; then
   echo "Success."
 else
